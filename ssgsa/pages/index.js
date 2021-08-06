a2b7e2df7cca48fc8de01665a5797fac54cf4264
SanjanaMaheshwari <sanjana.maheshwari456@gmail.com>
2021-08-06 19:44:03 +0530

    Initial Commit

---
+++ ssgsa/pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      Sir Syed Global Scholar Award Application Portal 
+    </div>
+  )
+}
