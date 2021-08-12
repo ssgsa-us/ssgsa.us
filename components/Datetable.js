db58b203ff6a9b3e71657283a1c38078e5414c94
Harsh Taliwal <gj3374@myamu.ac.in>
2021-08-13 00:06:18 +0530

    Adds apply component (#13)

--- components/Datetable.js
+++ components/Datetable.js
@@ -1,52 +1,52 @@
 const Datetable = () => {
   return (
-    <div>
-      <h1 className="text-blue-900 text-3xl my-4 mx-2 font-extrabold">Dates of full term 2021-2022</h1>
-      <table  className="shadow-xl mx-2">
+    <div className = "w-full lg:mr-4">
+      <h1 className="text-blue-850 text-3xl mx-2 mb-4 font-extrabold">Dates of Full Term 2021-2022</h1>
+      <table className="w-full">
         <tbody>
           <tr>
-            <td className="bg-blue-900 text-white p-2">
+            <td className="bg-blue-850 text-white p-2 w-1/3">
               Opening date of Applications
             </td>
-            <td className="border border-gray-400 p-2">To be announced</td>
+            <td className="border-2 border-gray-300 p-2 w-2/3">To be announced</td>
           </tr>
           <tr>
-            <td className="bg-blue-900 text-white p-2">Closing date for queries</td>
-            <td className="border border-gray-400 p-2">To be announced</td>
+            <td className="bg-blue-850 text-white p-2">Closing date for queries</td>
+            <td className="border-2 border-gray-300 p-2">To be announced</td>
           </tr>
           <tr>
-            <td className="bg-blue-900 text-white p-2">
+            <td className="bg-blue-850 text-white p-2">
               Closing date of Applications
             </td>
-            <td className="border border-gray-400 p-2">To be announced</td>
+            <td className="border-2 border-gray-300 p-2">To be announced</td>
           </tr>
           <tr>
-            <td className="bg-blue-900 text-white p-2">
+            <td className="bg-blue-850 text-white p-2">
               Announcement of Shortlisted candidates
             </td>
-            <td className="border border-gray-400 p-2">To be announced</td>
+            <td className="border-2 border-gray-300 p-2">To be announced</td>
           </tr>
           <tr>
-            <td className="bg-blue-900 text-white p-2">Interviews</td>
-            <td className="border border-gray-400 p-2">To be announced</td>
+            <td className="bg-blue-850 text-white p-2">Interviews</td>
+            <td className="border-2 border-gray-300 p-2">To be announced</td>
           </tr>
           <tr>
-            <td className="bg-blue-900 text-white p-2">
+            <td className="bg-blue-850 text-white p-2">
               Announcement of Sir Syed Global Scholar Awardees 2022-23
             </td>
-            <td className="border border-gray-400 p-2">To be announced</td>
+            <td className="border-2 border-gray-300 p-2">To be announced</td>
           </tr>
           <tr>
-            <td className="bg-blue-900 text-white p-2">Orientation</td>
-            <td className="border border-gray-400 p-2">To be announced</td>
+            <td className="bg-blue-850 text-white p-2">Orientation</td>
+            <td className="border-2 border-gray-300 p-2">To be announced</td>
           </tr>
           <tr>
-            <td className="bg-blue-900 text-white p-2">Allotment of Mentors</td>
-            <td className="border border-gray-400 p-2">To be announced</td>
+            <td className="bg-blue-850 text-white p-2">Allotment of Mentors</td>
+            <td className="border-2 border-gray-300 p-2">To be announced</td>
           </tr>
           <tr>
-            <td className="bg-blue-900 text-white p-2">Start of Term</td>
-            <td className="border border-gray-400 p-2">To be announced</td>
+            <td className="bg-blue-850 text-white p-2">Start of Term</td>
+            <td className="border-2 border-gray-300 p-2">To be announced</td>
           </tr>
         </tbody>
       </table>
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,19 @@
-import Datetable from "../components/Datetable"
-import Footer from "../components/Footer"
-import Header from "../components/Header"
-import styles from "../styles/Home.module.css"
+import Apply from "../components/Apply";
+import Datetable from "../components/Datetable";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Header />
-      <Datetable />
+      <div className="mx-2 mt-10 lg:flex justify-around lg:mx-20">
+        <Datetable />
+        <Apply />
+      </div>
+
       <Footer />
     </div>
-  )
+  );
 }
