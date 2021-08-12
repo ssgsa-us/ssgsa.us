86686a57ab6a96c2cc8646d62b3b9adec584e716
SanjanaMaheshwari <sanjana.maheshwari456@gmail.com>
2021-08-12 13:47:27 +0530

    Adds Datetable component

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Datetable from '../components/Datetable'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      Sir Syed Global Scholar Award Application Portal 
+      <Datetable/>
     </div>
   )
 }
