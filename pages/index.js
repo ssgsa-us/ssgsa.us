e9c1cac888a6a06b974ca638b19c875511c2e29d
harshtaliwal30 <harshtaliwal@gmail.com>
2021-08-12 23:37:14 +0530

    Adds apply component

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,20 @@
-import Head from "next/head"
-import Image from "next/image"
-import Datetable from "../components/Datetable"
-import Footer from "../components/Footer"
-import styles from "../styles/Home.module.css"
+import Head from "next/head";
+import Image from "next/image";
+import Datetable from "../components/Datetable";
+import Apply from "../components/Apply";
+import Footer from "../components/Footer";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
       <div className={styles.container}>
-        <Datetable />
+        <div className="mx-2 lg:flex justify-around lg:mx-20">
+          <Datetable />
+          <Apply />
+        </div>
       </div>
       <Footer />
     </div>
-  )
+  );
 }
