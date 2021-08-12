a4600b5211335c1a9b9195f3eba365ae22b8acc4
Himanshu <himvarandani@gmail.com>
2021-08-12 19:09:47 +0530

    Update footer for mobile view

--- components/Footer.js
+++ components/Footer.js
@@ -1,12 +1,13 @@
 import Link from "next/link"
+import Image from "next/image"
 
 export default function Footer() {
   return (
-    <div className="grid sm:grid-cols-8 md:grid-cols-10 lg:grid-cols-12 text-white mt-5">
-      <div className="sm:col-span-2 flex items-center justify-center p-2">
-        <img src="/logo.png" className="max-h-24" />
+    <div className="grid grid-cols-3 sm:grid-cols-8 md:grid-cols-10 lg:grid-cols-12 text-white mt-5">
+      <div className="sm:col-span-2 hidden sm:flex items-center justify-center p-2">
+        <Image src="/logo.png" alt="Logo" width={110} height={110} />
       </div>
-      <div className="sm:col-span-2 md:col-span-4 lg:col-span-6 flex items-center bg-blue-850 p-3">
+      <div className="col-span-2 md:col-span-4 lg:col-span-6 flex items-center bg-blue-850 p-3">
         <p className="text-sm font-bold">
           Sir Syed
           <br />
@@ -15,7 +16,7 @@ export default function Footer() {
           Of North America
         </p>
       </div>
-      <div className="flex flex-col bg-blue-850 p-3">
+      <div className="flex flex-col items-end sm:items-start bg-blue-850 p-3">
         <p className="text-sm font-bold mb-1">Social</p>
         <Link href="https://twitter.com/">
           <a className="text-xs">Twitter</a>
@@ -30,7 +31,7 @@ export default function Footer() {
           <a className="text-xs">Instagram</a>
         </Link>
       </div>
-      <div className="sm:col-span-3 lg:col-span-3 flex flex-col bg-blue-850 p-3">
+      <div className="col-span-3 flex flex-col items-center sm:items-start bg-blue-850 p-3">
         <p className="text-sm font-bold mb-1">contact@ssgsa.us</p>
         <p className="text-xs mb-3">A 501(c) (3) Non-profit Organisation</p>
         <p className="text-xs">&copy; Copyright 2006-21</p>
