f59b48889b7cea34e7ea1b984946680bebbd1cdf
Sanjana Maheshwari <sanjana.maheshwari456@gmail.com>
2021-08-13 23:35:54 +0530

    fixes#5 : Adds scholar, counter component (#15)
    * Adds counter component
    
    * Adds Scholar component
    
    * Scholar image corrections
    
    * Update Scholar component

---
+++ components/Scholar.js
@@ -0,0 +1,41 @@
+import Image from "next/image";
+
+const Scholar = () => {
+  return (
+    <div>
+      <div className="text-blue-850 font-bold lg:text-3xl mt-10">
+        Our Scholars
+      </div>
+      <div className="container container max-w-full m-auto flex flex-wrap flex-col md:flex-row items-center justify-start">
+        <div className="w-full pt-4">
+          <div className="flex flex-col lg:flex-row overflow-hidden border-2 border-gray-300">
+            <Image
+              width={1050}
+              height={1050}
+              src="/scholar_one.jpg"
+              alt="Scholar image"
+            />
+            <div className="flex-col leading-normal bg-blue-850">
+              <div className=" justify-self-stretch text-blue-850 bg-white font-bold lg:text-3xl p-4">
+                Wasikul <br />Islam
+              </div>
+              <div className="justify-self-stretch text-sm text-white mt-2 mb-2 p-2">
+                A former SSGSA Scholar, Dr. Islam now holds a Ph.D. in
+                Experimental Particle Physics from Oklahoma State University,
+                USA. He is also a former SSGSA Chairperson.<br /> He will be joining
+                Department of Physics, University of Wisconsin-Madison, USA as a
+                Research Associate and on behalf of his new university he will
+                be continuing his works at the CERN Laboratory, Switzerland
+                while pursuing research on physics of the Higgs Boson and Dark
+                matter searches
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+      
+  );
+};
+
+export default Scholar;
