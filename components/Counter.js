792e0f824974b8081067a19aee588ca1da60d463
ayaankhan <ayaankhan98@gmail.com>
2021-08-30 14:17:52 +0530

    fix responsive issue

--- components/Counter.js
+++ components/Counter.js
@@ -2,16 +2,16 @@ import CountUp from 'react-countup';
 
 const Counter = () => {
     return (
-      <div className="flex mx-2 my-5 lg:my-40 md:my-14 lg:mx-20 justify-around lg:text-4xl md:text-2xl text-blue-850 font-bold"
-      style={{fontSize: "80px", display: "flex", flexWrap: "wrap" }} >
-        <div className="flex-shrink">
-          <CountUp start={0} end={20} duration={1} />+ <span style={{fontSize: "40px"}}>Countries</span>
+      <div className="flex mx-2 my-5 lg:my-40 md:my-14 lg:mx-20 justify-around lg:text-5xl md:text-3xl sm:text-2xl text-blue-850 font-bold"
+      style={{ display: "flex", flexWrap: "wrap" }} >
+        <div>
+          <CountUp start={0} end={20} duration={1} />+<br /> <span className="lg:text-3xl md:text-2xl sm:text-xl">Countries</span>
         </div>
-        <div className="flex-shrink">
-          <CountUp start={0} end={99} duration={1} />+ <span style={{fontSize: "40px"}}>Universities</span>
+        <div>
+          <CountUp start={0} end={99} duration={1} />+<br /> <span className="lg:text-3xl md:text-2xl sm:text-xl" >Universities</span>
         </div>
-        <div className="flex-shrink">
-          <CountUp start={0} end={119} duration={1} />+ <span style={{fontSize: "40px"}}>Scholars</span>
+        <div>
+          <CountUp start={0} end={119} duration={1} />+<br /><span className="lg:text-3xl md:text-2xl sm:text-xl" >Scholars</span>
         </div>
     </div>
     );
