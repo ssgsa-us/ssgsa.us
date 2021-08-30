53c5175bbe4d1c4990090f266782769687d8c702
Ayaan Khan <ayaankhan98@gmail.com>
2021-08-30 19:08:57 +0530

    Animated number counter (#37)
    * animated-number-counter
    
    * included dep. react-countup
    
    * responsive animated-counter over mobile view
    
    * fix responsive issue

--- components/Counter.js
+++ components/Counter.js
@@ -1,11 +1,20 @@
+import CountUp from 'react-countup';
+
 const Counter = () => {
     return (
-      <div className="flex mx-2 my-5 lg:my-40 md:my-14 lg:mx-20 justify-around lg:text-4xl md:text-2xl text-blue-850 font-bold">
-        <div className="flex-shrink "> &nbsp;&nbsp;&nbsp;&nbsp; {"22"} <br/> {"Countries"}</div>
-        <div className="flex-shrink">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{"100"} <br/> {"Universities"}</div>
-        <div className="flex-shrink">&nbsp;&nbsp;&nbsp;&nbsp;{"120"} <br/> {"Scholars"}</div>
-      </div>
+      <div className="flex mx-2 my-5 lg:my-40 md:my-14 lg:mx-20 justify-around lg:text-5xl md:text-3xl sm:text-2xl text-blue-850 font-bold"
+      style={{ display: "flex", flexWrap: "wrap" }} >
+        <div>
+          <CountUp start={0} end={20} duration={1} />+<br /> <span className="lg:text-3xl md:text-2xl sm:text-xl">Countries</span>
+        </div>
+        <div>
+          <CountUp start={0} end={99} duration={1} />+<br /> <span className="lg:text-3xl md:text-2xl sm:text-xl" >Universities</span>
+        </div>
+        <div>
+          <CountUp start={0} end={119} duration={1} />+<br /><span className="lg:text-3xl md:text-2xl sm:text-xl" >Scholars</span>
+        </div>
+    </div>
     );
   };
-  
-  export default Counter;
+
+export default Counter;
