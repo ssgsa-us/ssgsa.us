5cecc48264e36510fcf29fc37e43d3a02a206014
Ahraz <shamimahraz@gmail.com>
2021-09-26 19:10:00 +0530

    Floating news added

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,11 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Header />
+      <div className="bg-gray-200 text-blue-900 w-full mt-8 lg:mr-2 lg:p-2 p-1"  >
+  <marquee>
+    <p> SSGSA results (2021-2022) annonced </p>
+  </marquee>
+   </div>
       <UpperCarousel />
       <Counter />
       <div className="mx-2 mt-10 lg:flex justify-around lg:mx-20">
