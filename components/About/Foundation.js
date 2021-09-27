4cba2d90c38c234f98c50f16adb6aa03d52e1f2b
Himanshu <himvarandani@gmail.com>
2021-09-27 22:28:43 +0530

    Update indentation of foundation component

--- components/About/Foundation.js
+++ components/About/Foundation.js
@@ -1,51 +1,56 @@
-import Image from "next/image";
-import Link from "next/link";
-
 const Foundation = () => {
   return (
-    <div className=" text-black w-full lg:mr-4 lg:p-8 p-4">
-       <h1 className=" text-sm sm:text-xl md:text-2xl -my-8 bg-blue-850  mb-10  text-white text-center font-extrabold py-2 rounded-tl-3xl rounded-br-3xl ">Foundation of the Sir Syed Scholar Award</h1>
-    <div className=" mx-3 px-8 " >
-      <p>
-        The Sir Syed Global Scholar Award (SSGSA) is annually administered by the 
-        Sir Syed Education Society of North America, a 501 (C) (3) non-profit organization. 
-        The award is offered to students and alumni of the Aligarh Muslim University (AMU) 
-        from all faculties. 
-
-      </p>
-      <br />
-      <p>
-        This award was first given in 2006 as the Sir Syed Excellence in Science Award to 
-        serve the needs of AMU Students interested in achieving higher education in the 
-        United States. It was designed to provide AMU students and alumni with financial 
-        assistance towards standardized tests and application fees, and personalized mentoring 
-        throughout the process from selection of universities to refining personal essays and resumes. 
-        Under the SSGSA program, so far over 120 AMU students have been successfully placed at 
-        various prestigious universities around the world.
-      </p>
-      <br />
-      <p>
-      SSGSA has a committed team of volunteers who mentor students 
-      and work with them for up to two years before students successfully secure a 
-      position in a Master’s or PhD program. These volunteers who mentor students are 
-      experts in their field. After starting their degrees, the awardees often become 
-      mentors to other incoming students. SSGSA sincerely believes that such a practice 
-      creates an environment of grander participation and will result in a greater 
-      number of mentors to help AMU students. 
-      </p>
-      <br />
-      <p>
-      Several AMU faculty members, researchers, and company executives are also 
-      part of the SSGSA network. In the past, many have delivered seminars, webinars, 
-      workshops and presentations under the SSGSA banner to introduce AMU students to 
-      opportunities of higher education abroad. In addition to increased exposure, these events are 
-      helpful in keeping students motivated throughout the application process that is long, tedious and expensive. 
-      Many students get distracted and move elsewhere due to lack of continuous motivation. 
-      The SSGSA team aims to identify meritorious students, empower them with knowledge and financial assistance, 
-      and help them to fulfill their ambition of higher education by keeping them focused with continued engagement.
-      
-      </p>
-     </div>
+    <div className="text-black w-full lg:mr-4 lg:p-8 p-4">
+      <h1
+        className="text-sm sm:text-xl md:text-2xl -my-8 bg-blue-850  mb-10  text-white text-center font-extrabold py-2 rounded-tl-3xl rounded-br-3xl"
+      >
+        Foundation of the Sir Syed Scholar Award
+      </h1>
+      <div className="mx-3 px-8" >
+        <p>
+          The Sir Syed Global Scholar Award (SSGSA) is annually administered
+          by the Sir Syed Education Society of North America, a 501 (C) (3)
+          non-profit organization. The award is offered to students and alumni
+          of the Aligarh Muslim University (AMU) from all faculties. 
+        </p>
+        <br />
+        <p>
+          This award was first given in 2006 as the Sir Syed Excellence in
+          Science Award to serve the needs of AMU Students interested in
+          achieving higher education in the United States. It was designed
+          to provide AMU students and alumni with financial assistance towards
+          standardized tests and application fees, and personalized mentoring 
+          throughout the process from selection of universities to refining
+          personal essays and resumes. Under the SSGSA program, so far over
+          120 AMU students have been successfully placed at various prestigious
+          universities around the world.
+        </p>
+        <br />
+        <p>
+          SSGSA has a committed team of volunteers who mentor students and work
+          with them for up to two years before students successfully secure a 
+          position in a Master’s or PhD program. These volunteers who mentor
+          students are experts in their field. After starting their degrees,
+          the awardees often become mentors to other incoming students. SSGSA
+          sincerely believes that such a practice creates an environment of
+          grander participation and will result in a greater number of mentors
+          to help AMU students.
+        </p>
+        <br />
+        <p>
+          Several AMU faculty members, researchers, and company executives are
+          also part of the SSGSA network. In the past, many have delivered
+          seminars, webinars, workshops and presentations under the SSGSA banner
+          to introduce AMU students to opportunities of higher education abroad.
+          In addition to increased exposure, these events are helpful in keeping
+          students motivated throughout the application process that is long,
+          tedious and expensive. Many students get distracted and move elsewhere
+          due to lack of continuous motivation. The SSGSA team aims to identify
+          meritorious students, empower them with knowledge and financial
+          assistance, and help them to fulfill their ambition of higher education
+          by keeping them focused with continued engagement.
+        </p>
+      </div>
     </div>
   );
 };
