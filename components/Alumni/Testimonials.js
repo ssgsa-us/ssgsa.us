90af886fc4377a8f6fdf25d8e5f405ea87353831
Himanshu Varandani <44665645+himanshuvarandani@users.noreply.github.com>
2021-10-11 18:59:35 +0530

    Add alumni page (#59)
    * Add alumni page
    
    * Resolve key error
    
    * Update testimonials to carousel

---
+++ components/Alumni/Testimonials.js
@@ -0,0 +1,143 @@
+import Carousel from "react-elastic-carousel";
+import Image from "next/image"
+
+const Testimonials = () => {
+  const testimonials = [
+    {
+      name: "Dr. Mohsin Jahan Qazi",
+      position: "Postdoctoral Researcher, Harvard University, USA",
+      imageUrl: "/members/MohsinJahanQazi.jpg",
+      words: "Realizing the role which SSGSA award played in moving towards \
+        my career goals, I felt very enthusiastic to contribute to the \
+        organization in order to help it expand and help more and more \
+        students like me who aspire to receive quality education outside \
+        India. In the past few years of my association with SSGSA as a member \
+        of operational team, it has given me an opportunity of working with a \
+        team, collaborating with people from both in and outside India and \
+        most importantly communicating to students and planning their \
+        applications. I believe that while working as a team member of SSGSA, \
+        I have acquired important organizational skills, which are very \
+        beneficial for my personal development and would also prove handy in \
+        my professional career"
+    },
+    {
+      name: "Dr. Abdul Haleem Kidwai",
+      position: "Postdoctoral Researcher, University of Wisconsin-La Crosse, USA",
+      imageUrl: "/members/AbdulHaleemKidwai.jpg",
+      words: "In a world that is becoming increasingly divisive, antagonistic \
+        and conflict-ridden, values of collaboration and cooperation need to \
+        be actively promoted. SSGSA is playing a pivotal role in this by \
+        facilitating international exchange of students and scholars."
+    },
+    {
+      name: "Shujaut H. Bader",
+      position: "PhD, Iowa State University,USA",
+      imageUrl: "/members/ShujautHBader.jpg",
+      words: "Throughout my journey to get an admit in one of the best \
+        american universities and that too with an assistantship- about which \
+        I had never thought, my trust in Opportunity as well as in Destiny \
+        has been increasing to unimaginable levels. I just don't find enough \
+        words to express my gratitude to Almighty God for whatever He has \
+        bestowed upon me. And I also believe in all those hands(of God) here \
+        on earth which have steered me throughout this strenuous journey. I \
+        have no doubt when I say that SSGSA was one of those hands apart from \
+        many others including the family I was born into.\n Opportunity is \
+        what shapes our lives, something which dictates the path of our \
+        destiny and SSGSA is doing a wonderful job of providing the \
+        opportunity to young aspiring students when the dream of higher \
+        studies in USA seems a distant dream at beginning. As it is said that \
+        the first step takes all the courage, mentoring and guidance by SSGSA \
+        in the beginning matters a lot when people don't even know an iota of \
+        the application process.\n I would like to be a part of SSGSA family \
+        to help and mentor new students as a sort of payback to whatever it \
+        (SSGSA) has done for me and as a human who takes pride in helping \
+        others out."
+    },
+    {
+      name: "Wasikul Islam",
+      position: "PhD, Oklahoma State University,USA",
+      imageUrl: "/members/WasikulIslam.jpg",
+      words: "Being a SSGSA awardee and a proud member of the family, I feel \
+        very much privileged. SSGSA not only encouraged me to dream big but \
+        also helped me to pursue it. I am really amazed to observe the working \
+        style of this family and the extent to which they cooperate with the \
+        students.\n The struggles and achievements of the seniors from Aligarh \
+        fraternity were great source of motivations for me. I am thankful to \
+        SSGSA for providing me wonderful opportunities few years ago by \
+        selecting me as one of its scholars, so that I could come to the US \
+        for Ph.D. Perhaps, I could never afford this 'seemingly expensive' \
+        dream. It was SSGSA, which changed my perceptions, dreams and \
+        motivations of life."
+    },
+    {
+      name: "Dr. Mohammad Saleem",
+      position: "Postdoctoral Researcher, Vanderbilt University Medical Center, USA",
+      imageUrl: "/members/MohammadSaleem.jpg",
+      words: "During my application process, SSGSA team helped me in every \
+        aspect including editing my resume, preparing statement of purpose \
+        (SOP), and selecting universities. Since then, I have been working \
+        with SSGSA team and observed that every student selection for SSGSA \
+        awards is completely transparent and unbiased. I request everyone to \
+        support this wonderful initiative that will help underprivileged \
+        students to get quality education abroad."
+    },
+    {
+      name: "Dr. Mohsin Khan",
+      position: "Associate, Booz Allen Hamilton, Washington DC, USA",
+      imageUrl: "/members/MohsinKhan.jpg",
+      words: "Mentors can make profound positive impacts on the lives of their \
+        mentees; SSGSA program has changed lives of over hundred individuals \
+        including myself."
+    },
+    {
+      name: "Kashif Ahmad",
+      position: "MS, Purdue University, USA",
+      imageUrl: "/members/DrKashifAhmad.jpg",
+      words: "SSGSA makes it easy for a student by not only paying for tests \
+        and application fee, but providing the invaluable motivation and \
+        guidance which I believe is the most important component of studying \
+        abroad. Mentors of SSGSA believed in me and made me realize that the \
+        sky is indeed the limit. The hard work coupled with excellent \
+        mentoring landed me into the Master’s program at Purdue University, a \
+        highly-reputed US University."
+    },
+  ]
+
+  return (
+    <div id="Testimonials">
+      <h1 className="my-8 mx-4 sm:mx-12 lg:mx-20 bg-blue-850 lg:text-3xl text-2xl text-white text-center font-extrabold py-2 rounded-tl-3xl rounded-br-3xl">
+        Testimonials
+      </h1>
+
+      <div className="mx-2 sm:mx-6 lg:mx-10 my-8">
+        <Carousel itemsToShow={1} enableAutoPlay={true} autoPlaySpeed={5000}>
+          {testimonials.map((testimonial, index) => (
+            <div
+              className="flex flex-col md:flex-row items-center bg-red-850 rounded-tl-3xl rounded-br-3xl"
+              key={index}
+            >
+              <div
+                className="flex flex-col items-center justify-center text-center m-2 p-2 md:w-1/3 lg:w-1/4"
+              >
+                <div className="relative rounded-full border-4 border-white overflow-hidden p-0 w-40 h-40">
+                  <Image
+                    src={testimonial.imageUrl}
+                    alt={testimonial.name}
+                    layout="fill"
+                  />
+                </div>
+                <p className="font-bold text-white sm:text-lg">{testimonial.name}</p>
+                <p className="text-white text-xs lg:text-sm">{testimonial.position}</p>
+              </div>
+              <div className="flex items-center text-white text-xs sm:text-sm m-4 sm:m-8 md:w-2/3 lg:w-3/4">
+                <p>{testimonial.words}</p>
+              </div>
+            </div>
+          ))}
+        </Carousel>
+      </div>
+    </div>
+  )
+}
+
+export default Testimonials
