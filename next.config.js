f04a9c1f299abc129008b2fcc6731198b2def5e0
Himanshu <himvarandani@gmail.com>
2021-10-26 22:14:09 +0530

    Send mail on submission of contact form

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
+require('dotenv').config()
+
 module.exports = {
+  env: {
+    EMAILJS_SERVICE_ID: process.env.EMAILJS_SERVICE_ID,
+    EMAILJS_TEMPLATE_ID: process.env.EMAILJS_TEMPLATE_ID,
+    EMAILJS_USER_ID: process.env.EMAILJS_USER_ID
+  },
   reactStrictMode: true,
 }
