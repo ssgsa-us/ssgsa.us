e68b4f0f7077166596ada6d8b23e20f5ab2ab7ce
Himanshu <himvarandani@gmail.com>
2021-10-31 00:58:48 +0530

    Add all constant values to a single folder

--- components/UpperCarousel.js
+++ components/UpperCarousel.js
@@ -1,28 +1,8 @@
 import Image from "next/image";
 import Carousel from "react-elastic-carousel";
+import { CarouselData } from "../constants/upperCarouselData";
 
 const UpperCarousel = () => {
-  const CarouselData = [
-    {
-      imgSrc: "/Convention_photo5.png",
-      imgAlt: "Convention Photo",
-      heading: " SSGSA success stories spread around globe",
-      width: 475,
-      height: 250,
-      content:
-        "120 SSGSA scholars joined 110 universities in 22 countries around globe, it was reported during SSGSA Convention 2020",
-    },
-    {
-      imgSrc: "/Convention_Collage.png",
-      imgAlt: "Convention Photo",
-      heading: "SSGSA Convention 2020",
-      width: 1000,
-      height: 550,
-      content:
-        "SSGSA Convention 2020 concluded successfully with the participations of 2019 Nobel Prize winner, Prof. Abhijit Banerjee, award winning actress Ms. Swara Bhaskar, Hon’ble Vice Chancellor of AMU, Prof. Tariq Mansoor & Noted Philanthropist Dr. Frank Islam.",
-    },
-  ];
-
   return (
     <Carousel itemsToShow={1}>
       {CarouselData.map((data, index) => {
