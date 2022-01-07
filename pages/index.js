7c7b1973c5b9f46522ca2bdd886771f044592165
Himanshu <himvarandani@gmail.com>
2022-01-07 20:00:05 +0530

    Add padding to main layout and update marquee

--- pages/index.js
+++ pages/index.js
@@ -8,14 +8,14 @@ import UpperCarousel from '../components/UpperCarousel'
 export default function Home() {
   return (
     <MainLayout>
-      <div className="bg-gray-200 text-blue-900 w-full mt-8 lg:mr-2 lg:p-2 p-1">
-        <marquee>
+      <div className="bg-gray-350 text-lg text-gray-850 font-black w-screen -ml-2 sm:-ml-5 md:-ml-10 lg:-ml-20 xl:-ml-40 mt-8 lg:mr-2 lg:p-3 p-2">
+        <marquee className="flex items-center">
           <p> SSGSA results (2021-2022) announced </p>
         </marquee>
       </div>
       <Counter />
       <UpperCarousel />
-      <div className="mx-2 mt-10 lg:flex justify-around lg:mx-20">
+      <div className="mx-2 mt-10 lg:flex justify-around">
         <div className="w-full lg:mr-4">
           <h1 className="text-blue-850 lg:text-3xl mx-2 mb-4 font-extrabold">
             Dates of Full Term 2021-2022
