33fee9b8caaac1ea1bb7464b82aee2dddf695ddf
Himanshu <himvarandani@gmail.com>
2022-01-24 20:27:31 +0530

    Add news and events page

---
+++ pages/news.tsx
@@ -0,0 +1,11 @@
+import MainLayout from '../layouts/Main'
+
+export default function Custom404() {
+  return (
+    <MainLayout>
+      <div className="flex justify-center items-center text-3xl text-red-850 my-52">
+        <p>Coming Soon!!</p>
+      </div>
+    </MainLayout>
+  )
+}
