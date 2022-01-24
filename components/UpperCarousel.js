a48cd213a7d76f94a421b7ab53c36e0544440a67
Himanshu <himvarandani@gmail.com>
2022-01-24 17:09:58 +0530

    Make image vertically centered in upper carousel

--- components/UpperCarousel.js
+++ components/UpperCarousel.js
@@ -29,15 +29,19 @@ const UpperCarousel = () => {
             className="bg-blue-850 text-white flex my-5 py-10 sm:py-0 flex-col sm:flex-row"
             key={index}
           >
-            <div className="mx-5 mb-5 sm:my-5 relative sm:w-1/2">
-              <Image
-                className="rounded-tl-3xl rounded-br-3xl"
-                layout="responsive"
-                src={data.imgSrc}
-                alt={data.imgAlt}
-                width={data.width}
-                height={data.height}
-              />
+            <div className="mx-5 mb-5 sm:my-5 sm:w-1/2 table">
+              <div className="table-cell align-middle">
+                <div className="relative">
+                  <Image
+                    className="rounded-tl-3xl rounded-br-3xl"
+                    layout="responsive"
+                    src={data.imgSrc}
+                    alt={data.imgAlt}
+                    width={data.width}
+                    height={data.height}
+                  />
+                </div>
+              </div>
             </div>
             <div className="mx-5 flex flex-col justify-center sm:py-10 sm:w-1/2">
               <h2 className="text-2xl font-black">{data.heading}</h2>
