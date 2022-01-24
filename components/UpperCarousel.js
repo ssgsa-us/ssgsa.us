a95ef410396600a62df991f15981300b50bdbfad
Himanshu <himvarandani@gmail.com>
2022-01-24 21:47:11 +0530

    Update upper carousel

--- components/UpperCarousel.js
+++ components/UpperCarousel.js
@@ -29,7 +29,7 @@ const UpperCarousel = () => {
             className="bg-blue-850 text-white flex my-5 py-10 sm:py-0 flex-col sm:flex-row"
             key={index}
           >
-            <div className="mx-5 mb-5 sm:my-5 sm:w-1/2 table">
+            <div className="mx-5 mb-5 sm:my-5 sm:w-1/2 xl:w-1/3 table">
               <div className="table-cell align-middle">
                 <div className="relative">
                   <Image
@@ -43,7 +43,7 @@ const UpperCarousel = () => {
                 </div>
               </div>
             </div>
-            <div className="mx-5 flex flex-col justify-center sm:py-10 sm:w-1/2">
+            <div className="mx-5 flex flex-col justify-center sm:py-10 sm:w-1/2 xl:w-2/3">
               <h2 className="text-2xl font-black">{data.heading}</h2>
               <p className="mt-3">{data.content}</p>
             </div>
