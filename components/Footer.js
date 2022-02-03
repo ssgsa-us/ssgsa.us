4cbfabd5cb24d81d412dc46538f53c5ba3f41770
Himanshu <himvarandani@gmail.com>
2022-02-03 22:34:42 +0530

    Update links target to "_blank"

--- components/Footer.js
+++ components/Footer.js
@@ -25,16 +25,24 @@ export default function Footer() {
         <div className="flex flex-col items-end sm:items-start bg-blue-850 p-3">
           <p className="text-sm font-bold mb-1">Social</p>
           <Link href="https://www.twitter.com/SirSyedGSA">
-            <a className="text-xs">Twitter</a>
+            <a className="text-xs" target="_blank">
+              Twitter
+            </a>
           </Link>
           <Link href="https://www.facebook.com/ssesapage">
-            <a className="text-xs">Facebook</a>
+            <a className="text-xs" target="_blank">
+              Facebook
+            </a>
           </Link>
           <Link href="https://www.linkedin.com/company/sir-syed-global-scholar-award-ssgsa">
-            <a className="text-xs">Linkedin</a>
+            <a className="text-xs" target="_blank">
+              Linkedin
+            </a>
           </Link>
           <Link href="https://www.instagram.com/sirsyedgsa">
-            <a className="text-xs">Instagram</a>
+            <a className="text-xs" target="_blank">
+              Instagram
+            </a>
           </Link>
         </div>
         <div className="col-span-3 flex flex-col items-center sm:items-start bg-blue-850 p-3">
