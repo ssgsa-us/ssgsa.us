724db3dc0f952cd73aed677624a4ab9e08f7913c
Himanshu Varandani <44665645+himanshuvarandani@users.noreply.github.com>
2022-02-03 11:58:26 +0530

    Update scholar component to carousel (#99)

--- components/Scholar.js
+++ components/Scholar.js
@@ -1,40 +1,59 @@
 import Image from 'next/image'
-import scholar_one from '../public/scholar_one.jpg'
+import { useRef } from 'react'
+import Carousel from 'react-elastic-carousel'
+import { Scholars } from '../constants/scholars'
 
 const Scholar = () => {
+  const carouselRef = useRef(null)
+  let resetTimeout
+
   return (
     <div>
-      <div className="text-blue-850 font-bold text-xl lg:text-2xl mt-10">
+      <div className="text-blue-850 font-bold text-xl lg:text-2xl mt-10 mb-5">
         Our Scholars
       </div>
-      <div className="container max-w-full m-auto flex flex-wrap flex-col sm:flex-row items-center justify-start">
-        <div className="w-full pt-4">
-          <div className="flex flex-col sm:flex-row overflow-hidden border-2 border-gray-300">
-            <div className="sm:w-1/2 h-full">
-              <Image
-                layout="responsive"
-                src={scholar_one}
-                alt="Scholar image"
-              />
-            </div>
-            <div className="flex-col leading-normal bg-blue-850 sm:w-1/2 sm:border-l-2 sm:border-gray-300">
-              <div className="justify-self-stretch text-blue-850 bg-white font-bold text-lg lg:text-xl p-4">
-                Wasikul Islam
+      <Carousel
+        ref={carouselRef}
+        itemsToShow={1}
+        enableAutoPlay={true}
+        autoPlaySpeed={5000}
+        showArrows={false}
+        onNextEnd={({ index }) => {
+          clearTimeout(resetTimeout)
+          if (index === Scholars.length - 1) {
+            resetTimeout = setTimeout(() => {
+              carouselRef.current.goTo(0)
+            }, 5000)
+          }
+        }}
+      >
+        {Scholars.map((scholar, index) => {
+          return (
+            <div
+              className="flex flex-col sm:flex-row items-center bg-blue-850 rounded-tl-3xl rounded-br-3xl p-1"
+              key={index}
+            >
+              <div className="flex flex-col items-center justify-center m-2 p-2 sm:w-2/5">
+                <div className="relative rounded-full border-4 border-white overflow-hidden p-0 w-40 h-40">
+                  <Image
+                    src={scholar.imageUrl}
+                    alt={scholar.name}
+                    layout="fill"
+                  />
+                </div>
               </div>
-              <div className="justify-self-stretch text-sm lg:text-xs text-white mt-2 mb-2 p-2">
-                A former SSGSA Scholar, Dr. Islam now holds a Ph.D. in
-                Experimental Particle Physics from Oklahoma State University,
-                USA. He is also a former SSGSA Chairperson.
-                <br /> He will be joining Department of Physics, University of
-                Wisconsin-Madison, USA as a Research Associate and on behalf of
-                his new university he will be continuing his works at the CERN
-                Laboratory, Switzerland while pursuing research on physics of
-                the Higgs Boson and Dark matter searches
+              <div className="flex flex-col h-full text-white sm:w-4/5">
+                <div className="bg-white text-center w-full py-2">
+                  <p className="font-bold text-blue-850 text-lg">
+                    {scholar.name}
+                  </p>
+                </div>
+                <p className="text-xs m-2 mb-4 sm:ml-0">{scholar.words}</p>
               </div>
             </div>
-          </div>
-        </div>
-      </div>
+          )
+        })}
+      </Carousel>
     </div>
   )
 }
