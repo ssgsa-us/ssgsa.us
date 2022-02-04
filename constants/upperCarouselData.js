5991a5cf32bccb9f53357ef118b9ca98b03968c9
Himanshu <himvarandani@gmail.com>
2022-02-04 11:49:58 +0530

    Add workshop data to carousel

--- constants/upperCarouselData.js
+++ constants/upperCarouselData.js
@@ -17,4 +17,13 @@ export const CarouselData = [
     content:
       'The SSGSA convention 2020 was a great success and featured talks from the Nobel laureate Prof. Abhijit Banerjee, award winning actress Ms. Swara Bhasker, Hon’ble Vice Chancellor of AMU, Prof. Tariq Mansoor & Noted Philanthropist Dr. Frank Islam.',
   },
+  {
+    imgSrc: '/MolecularWorkshop2022.png',
+    imgAlt: 'Molecular Modeling Workshop 2022 Photo',
+    heading: 'Molecular Modeling Workshop 2022',
+    width: 475,
+    height: 250,
+    content:
+      'The training and skill development division at SSGSA has successfully organized its first technical workshop, virtually on 29-30.01.2022. The workshop participants had "hands-on" training using commonly used software related to molecular biology during the practical session of two hours each day.',
+  },
 ]
