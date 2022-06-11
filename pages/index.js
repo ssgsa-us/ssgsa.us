f3a0555e43560de27f14d40b38c2afe62dc4ee0a
Prajjwal Nijhara <prajjwalnijhara@gmail.com>
2022-06-11 17:04:10 +0530

    Update news headline

--- pages/index.js
+++ pages/index.js
@@ -11,11 +11,9 @@ export default function Home() {
     <MainLayout>
       <div className="bg-gray-350 text-red-850 font-black w-screen -ml-2 sm:-ml-5 md:-ml-10 lg:-ml-20 xl:-ml-40 mt-8 lg:mr-2 lg:p-3 p-2">
         <marquee className="flex items-center">
-          <Link href="/application-portal">
+          <Link href="/assets/PressRelease-SSGSA-2021-22.pdf">
             <a>
-              The interviews for the current session will take place on May 28
-              and 29. The final list of awardees will be released soon. Stay
-              tuned
+              SSGSA results (2021-2022) announced
             </a>
           </Link>
         </marquee>
