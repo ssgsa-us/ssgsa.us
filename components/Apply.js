012c7e6abb0c21c4e48a5635157f8f27e99a0c78
Himanshu <himvarandani@gmail.com>
2022-08-20 17:32:14 +0530

    Update home page
    Add new section about SSGSA on home page and remove 'Ask a question' button

--- components/Apply.js
+++ components/Apply.js
@@ -4,7 +4,7 @@ import Link from 'next/dist/client/link'
 const Apply = () => {
   return (
     <div className="mt-8 lg:mt-0 lg:ml-4 lg:w-1/3 flex justify-center">
-      <div className="w-4/5 sm:w-3/4 md:w-3/5 lg:w-full bg-gray-100">
+      <div className="w-4/5 sm:w-3/4 md:w-3/5 lg:w-full bg-gray-100 pb-8">
         <div className="mx-8 pt-3">
           <Link href="/application-portal">
             <button className="px-10 py-1 block mx-auto bg-red-850 font-black text-lg text-white text-center">
@@ -109,12 +109,6 @@ const Apply = () => {
             Press Release Statement of SSGSA Convention 2020
           </button>
         </a>
-
-        <div className="mx-4">
-          <button className="px-2 py-2 mt-8 mb-8 w-full bg-blue-850 text-sm text-white text-center rounded-tl-md rounded-br-md">
-            Ask a Question
-          </button>
-        </div>
       </div>
     </div>
   )
--- pages/index.js
+++ pages/index.js
@@ -12,14 +12,27 @@ export default function Home() {
       <div className="bg-gray-350 text-red-850 font-black w-screen -ml-2 sm:-ml-5 md:-ml-10 lg:-ml-20 xl:-ml-40 mt-8 lg:mr-2 lg:p-3 p-2">
         <marquee className="flex items-center">
           <Link href="/assets/PressRelease_SSGSA_2022_23.pdf">
-            <a>
-              SSGSA results (2022-2023) announced
-            </a>
+            <a>SSGSA results (2022-2023) announced</a>
           </Link>
         </marquee>
       </div>
       <Counter />
       <UpperCarousel />
+      <div className="bg-blue-850 rounded-xl text-white mx-2 my-10 p-5 sm:px-10">
+        <h1 className="text-2xl font-black">What is SSGSA?</h1>
+        <p className="text-lg mt-3">
+          An Aligarh Muslim University (AMU) alumni initiative for mentoring and
+          financially supporting meritorious AMU students for grad school
+          applications.
+        </p>
+        <p className="text-sm mt-2">
+          Read more about SSGSA{' '}
+          <Link href="/about">
+            <a className="text-red-500">here</a>
+          </Link>
+          .
+        </p>
+      </div>
       <div className="mx-2 mt-10 lg:flex justify-around">
         <div className="w-full lg:mr-4">
           <h1 className="text-blue-850 text-xl lg:text-2xl mx-2 mb-4 font-extrabold">
