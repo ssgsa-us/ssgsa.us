e6d9acaa35b4dfaf44d18f93711af1f7b713f667
Himanshu <himvarandani@gmail.com>
2022-12-15 16:43:54 +0530

    Add application step 6

---
+++ pages/api/step6.tsx
@@ -0,0 +1,14 @@
+import path from 'path'
+import { firestore } from '../../firebase'
+import { CurricularActivitiesType } from '../../types'
+
+export const updateApplicationData = (
+  userId: string,
+  curricularActivities: CurricularActivitiesType,
+  formStatus: number,
+) => {
+  return firestore.doc(path.join('applications_data', userId)).update({
+    curricular_activities: curricularActivities,
+    form_status: formStatus,
+  })
+}
