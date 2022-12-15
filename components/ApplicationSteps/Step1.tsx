b0879144657dc06e27913e8f038b7869ef2b803e
Himanshu <himvarandani@gmail.com>
2022-12-16 03:11:37 +0530

    Use text and select input component in step 1

--- components/ApplicationSteps/Step1.tsx
+++ components/ApplicationSteps/Step1.tsx
@@ -4,6 +4,8 @@ import ProceedButtons from './ProceedButtons'
 import { updateApplicationData } from '../../pages/api/step1'
 import { useAuth } from '../../context/AuthUserContext'
 import FileUploadComponent from './FileUpload'
+import TextInput from './TextInput'
+import SelectInput from './SelectInput'
 
 type Props = {
   applicationData: ApplicationData
@@ -32,7 +34,7 @@ const Step1 = ({ applicationData, status, setStatus }: Props) => {
     setContactNo(applicationData.contact || 0)
     setCurrentPosition(applicationData.current_position || '')
     setTargetProgram(applicationData.target_program || '')
-    setTargetDate(applicationData.target_date || 'In 0-1 year')
+    setTargetDate(applicationData.target_date || '')
     setTargetCountry(applicationData.target_country || '')
   }, [applicationData])
 
@@ -114,31 +116,20 @@ const Step1 = ({ applicationData, status, setStatus }: Props) => {
           Note: Remember to save your information at frequent intervals.
         </p>
         <br />
-        <div className="p-2">
-          <p className="md:text-lg">
-            Name<span className="text-red-850 font-black">*</span>
-          </p>
-          <input
-            name="Name"
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            className="w-full rounded-xl p-2 mt-1"
-          />
-        </div>
-        <div className="p-2">
-          <p className="md:text-lg">
-            AMU Enrollment Number
-            <span className="text-red-850 font-black">*</span>
-          </p>
-          <input
-            name="EnrollNo"
-            type="text"
-            value={enrollNo}
-            onChange={(e) => setEnrollNo(e.target.value)}
-            className="w-full rounded-xl p-2 mt-1"
-          />
-        </div>
+        <TextInput
+          name="Name"
+          value={name}
+          type="text"
+          onChange={(e) => setName(e.target.value)}
+          required={true}
+        />
+        <TextInput
+          name="AMU Enrollment Number"
+          value={enrollNo}
+          type="text"
+          onChange={(e) => setEnrollNo(e.target.value)}
+          required={true}
+        />
         <div className="p-2">
           <p className="md:text-lg">
             AMU Enrollment Proof Document
@@ -155,103 +146,63 @@ const Step1 = ({ applicationData, status, setStatus }: Props) => {
             setFileUrl={(url: string) => setEnrollProofDoc(url)}
           />
         </div>
-        <div className="p-2">
-          <p className="md:text-lg leading-none">
-            E-mail address<span className="text-red-850 font-black">*</span>{' '}
-            <span className="text-xs md:text-sm">
-              (this email ID will stay in the SSGSA records and be used for
-              future correspondence)
-            </span>
-          </p>
-          <input
-            name="Email"
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="w-full rounded-xl p-2 mt-1"
-          />
-        </div>
-        <div className="p-2">
-          <p className="md:text-lg leading-none">
-            Contact Number<span className="text-red-850 font-black">*</span>
-          </p>
-          <input
-            name="ContactNumber"
-            type="number"
-            value={contactNo}
-            onChange={(e) => setContactNo(Number(e.target.value))}
-            className="w-full rounded-xl p-2 mt-1"
-          />
-        </div>
-        <div className="p-2">
-          <p className="md:text-lg">
-            Current Position<span className="text-red-850 font-black">*</span>
-            <span className="text-xs md:text-sm">
-              <br />
-              (e.g., Final year BTech student at AMU, Software Developer in
-              Indian Railways, Taking a year gap, Preparing for entrance exams
-              like GATE etc.)
-            </span>
-          </p>
-          <input
-            name="Current Position"
-            type="text"
-            value={currentPosition}
-            onChange={(e) => setCurrentPosition(e.target.value)}
-            className="w-full rounded-xl p-2 mt-1"
-          />
-        </div>
-        <div className="p-2">
-          <p className="md:text-lg">
-            Which program(s) and in what subject(s) are you interested in
-            studying abroad?<span className="text-red-850 font-black">*</span>
-            <span className="text-xs md:text-sm">
-              <br />
-              (e.g., MS in Machine Learning, MBA, PhD in Asian History, etc.)
-            </span>
-          </p>
-          <input
-            name="Target Program"
-            type="text"
-            value={targetProgram}
-            onChange={(e) => setTargetProgram(e.target.value)}
-            className="w-full rounded-xl p-2 mt-1"
-          />
-        </div>
-        <div className="p-2">
-          <p className="md:text-lg">
-            When do you plan to apply for grad school?
-            <span className="text-red-850 font-black">*</span>
-          </p>
-          <select
-            name="Target Date"
-            value={targetDate}
-            onChange={(e) => setTargetDate(e.target.value)}
-            className="w-full rounded-xl p-3"
-          >
-            <option label="In 0-1 year" value="In 0-1 year" />
-            <option label="In 1-2 years" value="In 1-2 years" />
-            <option label="more than 2 years" value="more than 2 years" />
-            <option label="not decided yet" value="not decided yet" />
-          </select>
-        </div>
-        <div className="p-2">
-          <p className="md:text-lg">
-            Which country(s) will you be applying in?
-            <span className="text-red-850 font-black">*</span>
-            <span className="text-xs md:text-sm">
-              <br />
-              You can list more than one here.
-            </span>
-          </p>
-          <input
-            name="Target Country"
-            type="text"
-            value={targetCountry}
-            onChange={(e) => setTargetCountry(e.target.value)}
-            className="w-full rounded-xl p-2 mt-1"
-          />
-        </div>
+        <TextInput
+          name="E-mail address"
+          description="(this email ID will stay in the SSGSA records and be 
+            used for future correspondence)"
+          value={email}
+          type="text"
+          onChange={(e) => setEmail(e.target.value)}
+          required={true}
+        />
+        <TextInput
+          name="Contact Number"
+          value={contactNo}
+          type="number"
+          onChange={(e) => setContactNo(Number(e.target.value))}
+          required={true}
+        />
+        <TextInput
+          name="Current Position"
+          description="(e.g., Final year BTech student at AMU, Software 
+            Developer in Indian Railways, Taking a year gap, Preparing for 
+            entrance exams like GATE etc.)"
+          value={currentPosition}
+          type="text"
+          onChange={(e) => setCurrentPosition(e.target.value)}
+          required={true}
+        />
+        <TextInput
+          name="Which program(s) and in what subject(s) are you interested in
+            studying abroad?"
+          description="(e.g., MS in Machine Learning, MBA, PhD in Asian 
+            History, etc.)"
+          value={targetProgram}
+          type="text"
+          onChange={(e) => setTargetProgram(e.target.value)}
+          required={true}
+        />
+        <SelectInput
+          name="When do you plan to apply for grad school?"
+          value={targetDate}
+          options={[
+            { label: 'Select', value: '' },
+            { label: 'In 0-1 year', value: 'In 0-1 year' },
+            { label: 'In 1-2 years', value: 'In 1-2 years' },
+            { label: 'more than 2 years', value: 'more than 2 years' },
+            { label: 'not decided yet', value: 'not decided yet' },
+          ]}
+          onChange={(e) => setTargetDate(e.target.value)}
+          required={true}
+        />
+        <TextInput
+          name="Which country(s) will you be applying in?"
+          description="You can list more than one here."
+          value={targetCountry}
+          type="text"
+          onChange={(e) => setTargetCountry(e.target.value)}
+          required={true}
+        />
       </div>
       <ProceedButtons
         status={status}
