0ca40cceaa80a0d4c21dc17660def4ca5e4a16e8
Himanshu <himvarandani@gmail.com>
2022-12-25 22:38:49 +0530

    Update home page marquee

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       <div className="bg-gray-350 text-red-850 font-black w-screen -ml-2 sm:-ml-5 md:-ml-10 lg:-ml-20 xl:-ml-40 mt-8 lg:mr-2 lg:p-3 p-2">
         <marquee className="flex items-center">
           <Link href="/assets/PressRelease_SSGSA_2022_23.pdf">
-            <a>SSGSA results (2022-2023) announced</a>
+            <a>SSGSA Application 2023-24 will be opening soon</a>
           </Link>
         </marquee>
       </div>
