2baf9cd9a813946195740500818682cb9c439c04
Himanshu <himvarandani@gmail.com>
2022-12-26 20:47:59 +0530

    Create step 5 for review application

--- components/ApplicationSteps/ReviewApplication.tsx
+++ components/ApplicationSteps/ReviewApplication.tsx
@@ -4,6 +4,7 @@ import Step1 from '../ReviewApplicationSteps/Step1'
 import Step2 from '../ReviewApplicationSteps/Step2'
 import Step3 from '../ReviewApplicationSteps/Step3'
 import Step4 from '../ReviewApplicationSteps/Step4'
+import Step5 from '../ReviewApplicationSteps/Step5'
 
 type Props = {
   applicationData: ApplicationData
@@ -19,6 +20,7 @@ const ReviewApplication = ({ applicationData }: Props) => {
       <Step2 academicRecord={applicationData.academic_record} />
       <Step3 researchData={applicationData.research_experience} />
       <Step4 workExperiences={applicationData.work_experience} />
+      <Step5 workshops={applicationData.poster_or_workshops} />
       <div className="bg-gray-200 rounded-3xl py-5 px-3 sm:py-10 sm:px-10">
         <div className="mb-10">
           <p className="text-black text-xl sm:text-2xl font-extrabold mb-4">
---
+++ components/ReviewApplicationSteps/Step5.tsx
@@ -0,0 +1,43 @@
+import { PosterOrWorkshopsType } from '../../types'
+import FileField from './FIleField'
+import Field from './Field'
+
+type Props = {
+  workshops: PosterOrWorkshopsType
+}
+
+const Step5 = ({ workshops }: Props) => (
+  <div className="bg-gray-200 rounded-3xl py-5 px-3 sm:py-10 sm:px-10 my-5">
+    <h1 className="text-xl sm:text-2xl text-center font-bold pb-5">
+      Poster Presentation/Workshops/Summer School
+    </h1>
+    {!workshops || !Object.keys(workshops).length ? (
+      <p className="font-bold mb-4">No Poster/Workshop</p>
+    ) : (
+      Object.keys(workshops).map((key, index) => (
+        <div className="mb-10" key={key}>
+          <p className="text-black text-lg sm:text-xl font-extrabold">
+            Poster/Workshop {index + 1}
+          </p>
+          <Field name="Category" value={workshops[Number(key)].category} />
+          <Field name="Title" value={workshops[Number(key)].title} />
+          <Field
+            name="Years Attended and Duration"
+            value={workshops[Number(key)].duration}
+          />
+          <Field
+            name="Description"
+            value={workshops[Number(key)].description}
+          />
+          <FileField
+            fieldName="Corresponding Document"
+            fileName={`PosterOrWorkshop${key}.pdf`}
+            url={workshops[Number(key)].document}
+          />
+        </div>
+      ))
+    )}
+  </div>
+)
+
+export default Step5
