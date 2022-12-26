91564e90b0dee9b538fa28cc046006bb8ca520c3
Himanshu <himvarandani@gmail.com>
2022-12-26 20:59:10 +0530

    Create step 6 for review application

--- components/ApplicationSteps/ReviewApplication.tsx
+++ components/ApplicationSteps/ReviewApplication.tsx
@@ -5,6 +5,7 @@ import Step2 from '../ReviewApplicationSteps/Step2'
 import Step3 from '../ReviewApplicationSteps/Step3'
 import Step4 from '../ReviewApplicationSteps/Step4'
 import Step5 from '../ReviewApplicationSteps/Step5'
+import Step6 from '../ReviewApplicationSteps/Step6'
 
 type Props = {
   applicationData: ApplicationData
@@ -21,6 +22,7 @@ const ReviewApplication = ({ applicationData }: Props) => {
       <Step3 researchData={applicationData.research_experience} />
       <Step4 workExperiences={applicationData.work_experience} />
       <Step5 workshops={applicationData.poster_or_workshops} />
+      <Step6 curricularActivities={applicationData.curricular_activities} />
       <div className="bg-gray-200 rounded-3xl py-5 px-3 sm:py-10 sm:px-10">
         <div className="mb-10">
           <p className="text-black text-xl sm:text-2xl font-extrabold mb-4">
