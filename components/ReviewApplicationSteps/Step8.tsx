7a8631afe2a110927893c07b44eb9dfb72e97d6a
Himanshu <himvarandani@gmail.com>
2022-12-26 21:11:16 +0530

    Create step 8 for review application

---
+++ components/ReviewApplicationSteps/Step8.tsx
@@ -0,0 +1,34 @@
+import { AnswerType } from '../../types'
+import Field from './Field'
+
+type Props = {
+  sopAnswers: AnswerType
+}
+
+const Step8 = ({ sopAnswers }: Props) => (
+  <div className="bg-gray-200 rounded-3xl py-5 px-3 sm:py-10 sm:px-10 my-5">
+    <h1 className="text-xl sm:text-2xl text-center font-bold pb-5">Essays</h1>
+    <Field
+      name={`a) ${process.env.NEXT_PUBLIC_QUESTION_1}`}
+      value={sopAnswers['SOP1']}
+    />
+    <Field
+      name={`b) ${process.env.NEXT_PUBLIC_QUESTION_2}`}
+      value={sopAnswers['SOP2']}
+    />
+    <Field
+      name={`c) ${process.env.NEXT_PUBLIC_QUESTION_3}`}
+      value={sopAnswers['SOP3']}
+    />
+    <Field
+      name={`d) ${process.env.NEXT_PUBLIC_QUESTION_4}`}
+      value={sopAnswers['SOP4']}
+    />
+    <Field
+      name={`e) ${process.env.NEXT_PUBLIC_QUESTION_5}`}
+      value={sopAnswers['SOP5']}
+    />
+  </div>
+)
+
+export default Step8
