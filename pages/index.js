5e3cce07e0fecc0504010b9691e6bb14f97e9799
Zeba Karin Ahmad <zebakarin@gmail.com>
2023-01-02 19:50:21 +0100

    Change exact date to opening soon

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       <div className="bg-gray-350 text-red-850 font-black w-screen -ml-2 sm:-ml-5 md:-ml-10 lg:-ml-20 xl:-ml-40 mt-8 lg:mr-2 lg:p-3 p-2">
         <marquee className="flex items-center">
           <Link href="https://www.ssgsa.us/apply#WhoCanApply">
-            <a>Applications for the session 2023-24 open on Jan 15, 2023</a>
+            <a>Applications for the session 2023-24 will be opening soon</a>
           </Link>
         </marquee>
       </div>
