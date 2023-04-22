a598133628dcc18b3688fa70dcf6bee1c36078b2
Himanshu <himvarandani@gmail.com>
2023-04-23 04:27:13 +0530

    Update int view appl page with header

--- pages/interviewer/view-application/[applId].tsx
+++ pages/interviewer/view-application/[applId].tsx
@@ -2,16 +2,15 @@ import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 import { AdminPortalData } from '../../../classes/admin_portal_data'
 import { ApplicationData } from '../../../classes/application_data'
-import ReviewApplication from '../../../components/ApplicationSteps/ReviewApplication'
 import Loading from '../../../components/Loading'
 import requireAuth from '../../../components/requireAuth'
 import Roles from '../../../constants/roles'
 import { useAuth } from '../../../context/AuthUserContext'
-import InterviewerLayout from '../../../layouts/interviewer/interviewer-layout'
+import ApplicationLayout from '../../../layouts/interviewer/ApplicationLayout'
+import { InterviewerInstructionsType } from '../../../types'
 import { getAdminPortalData } from '../../api/getAdminPortalData'
 import { getApplicationData } from '../../api/getApplicationData'
-import { updateInterviewMarks } from '../../api/updateInterviewMarks'
-import { updateInterviewRemark } from '../../api/updateInterviewRemark'
+import { getInterviewerInstructions } from '../../api/instructions'
 
 function ViewApplication() {
   const { authUser } = useAuth()
@@ -19,26 +18,20 @@ function ViewApplication() {
     new AdminPortalData(),
   )
   const [applicationData, setApplicationData] = useState<ApplicationData>()
-  const [A, setA] = useState<number>(0)
-  const [B, setB] = useState<number>(0)
-  const [C, setC] = useState<number>(0)
-  const [D, setD] = useState<number>(0)
-  const [remark, setRemark] = useState<string>('')
-  const [changeOccured, setChangeOccured] = useState<boolean>(false)
+  const [formStatus, setFormStatus] = useState<number>(1)
+  const [status, setStatus] = useState<number>(1)
   const [pageReady, setPageReady] = useState<boolean>(false)
+  const [instructions, setInstructions] = useState<InterviewerInstructionsType>(
+    {},
+  )
   const router = useRouter()
   const applId = String(router.query['applId'])
 
-  const setInterviewerMarks = (data: AdminPortalData) => {
-    let interview_marks = data.interview_marks
-    if (interview_marks && interview_marks[authUser.id]) {
-      setA(0)
-      setB(0)
-      setC(0)
-      setD(0)
-      setRemark(interview_marks[authUser.id].remark || '')
-    }
-  }
+  useEffect(() => {
+    getInterviewerInstructions()
+      .then((data) => setInstructions(data))
+      .catch(() => alert('Not able to fetch instructions, Try reloading!'))
+  }, [])
 
   useEffect(() => {
     if (applId) {
@@ -49,7 +42,14 @@ function ViewApplication() {
             .then((data: AdminPortalData) => {
               if (data) {
                 setAdminPortalData(data)
-                setInterviewerMarks(data)
+                if (
+                  data.interview_marks &&
+                  data.interview_marks[authUser.id] &&
+                  data.interview_marks[authUser.id].formStatus
+                ) {
+                  setStatus(data.interview_marks[authUser.id].formStatus)
+                  setFormStatus(data.interview_marks[authUser.id].formStatus)
+                }
               }
             })
             .catch(() => alert('Try again, network error!'))
@@ -57,236 +57,64 @@ function ViewApplication() {
         .catch(() => alert('Try again, network error!'))
         .finally(() => setPageReady(true))
     }
-  }, [router.query, changeOccured])
+  }, [router.query])
+
+  useEffect(() => {
+    if (applId && applicationData && applicationData.email) {
+      getAdminPortalData(applId)
+        .then((data: AdminPortalData) => {
+          if (data) {
+            setAdminPortalData(data)
+            if (
+              data.interview_marks &&
+              data.interview_marks[authUser.id] &&
+              data.interview_marks[authUser.id].formStatus
+            )
+              setFormStatus(data.interview_marks[authUser.id].formStatus)
+          }
+        })
+        .catch(() => alert('Try again, network error!'))
+    }
+  }, [status])
 
   return (
-    <InterviewerLayout>
+    <ApplicationLayout
+      status={status}
+      formStatus={formStatus}
+      setStatus={setStatus}
+    >
       {pageReady ? (
-        <div>
-          <h1 className="text-sm sm:text-xl md:text-2xl bg-blue-850 my-10 text-white text-center font-extrabold py-2 rounded-tl-3xl rounded-br-3xl">
-            View User Application
-          </h1>
-          {applicationData ? (
-            <div>
-              <ReviewApplication applicationData={applicationData} />
-              {applicationData.form_status == 6 ? (
-                <div>
-                  <div className="bg-gray-200 rounded-3xl py-5 px-3 sm:py-10 sm:px-10 my-10">
-                    <div className="flex justify-between my-5">
-                      <p className="text-red-850 text-lg sm:text-xl font-extrabold">
-                        {process.env.NEXT_PUBLIC_INTERVIEW_MARKS_INDEX_A} (Out
-                        of {process.env.NEXT_PUBLIC_INTERVIEW_INDEX_A_MAX_MARKS}
-                        )
-                      </p>
-                      <p className="sm:text-lg font-bold">
-                        <input
-                          name="Name"
-                          type="number"
-                          value={A}
-                          min={0}
-                          max={Number(
-                            process.env.NEXT_PUBLIC_INTERVIEW_INDEX_A_MAX_MARKS,
-                          )}
-                          onChange={(e) =>
-                            Number(e.target.value) >
-                            Number(
-                              process.env
-                                .NEXT_PUBLIC_INTERVIEW_INDEX_A_MAX_MARKS,
-                            )
-                              ? null
-                              : setA(Number(e.target.value))
-                          }
-                          className="w-full rounded-xl p-2 mt-1"
-                        />
-                      </p>
-                    </div>
-                    <div className="flex justify-between my-5">
-                      <p className="text-red-850 text-lg sm:text-xl font-extrabold">
-                        {process.env.NEXT_PUBLIC_INTERVIEW_MARKS_INDEX_B} (Out
-                        of {process.env.NEXT_PUBLIC_INTERVIEW_INDEX_B_MAX_MARKS}
-                        )
-                      </p>
-                      <p className="sm:text-lg font-bold">
-                        <input
-                          name="Name"
-                          type="number"
-                          value={B}
-                          min={0}
-                          max={Number(
-                            process.env.NEXT_PUBLIC_INTERVIEW_INDEX_B_MAX_MARKS,
-                          )}
-                          onChange={(e) =>
-                            Number(e.target.value) >
-                            Number(
-                              process.env
-                                .NEXT_PUBLIC_INTERVIEW_INDEX_B_MAX_MARKS,
-                            )
-                              ? null
-                              : setB(Number(e.target.value))
-                          }
-                          className="w-full rounded-xl p-2 mt-1"
-                        />
-                      </p>
-                    </div>
-                    <div className="flex justify-between my-5">
-                      <p className="text-red-850 text-lg sm:text-xl font-extrabold">
-                        {process.env.NEXT_PUBLIC_INTERVIEW_MARKS_INDEX_C} (Out
-                        of {process.env.NEXT_PUBLIC_INTERVIEW_INDEX_C_MAX_MARKS}
-                        )
-                      </p>
-                      <p className="sm:text-lg font-bold">
-                        <input
-                          name="Name"
-                          type="number"
-                          value={C}
-                          min={0}
-                          max={Number(
-                            process.env.NEXT_PUBLIC_INTERVIEW_INDEX_C_MAX_MARKS,
-                          )}
-                          onChange={(e) =>
-                            Number(e.target.value) >
-                            Number(
-                              process.env
-                                .NEXT_PUBLIC_INTERVIEW_INDEX_C_MAX_MARKS,
-                            )
-                              ? null
-                              : setC(Number(e.target.value))
-                          }
-                          className="w-full rounded-xl p-2 mt-1"
-                        />
-                      </p>
-                    </div>
-                    <div className="flex justify-between my-5">
-                      <p className="text-red-850 text-lg sm:text-xl font-extrabold">
-                        {process.env.NEXT_PUBLIC_INTERVIEW_MARKS_INDEX_D} (Out
-                        of {process.env.NEXT_PUBLIC_INTERVIEW_INDEX_D_MAX_MARKS}
-                        )
-                      </p>
-                      <p className="sm:text-lg font-bold">
-                        <input
-                          name="Name"
-                          type="number"
-                          value={D}
-                          min={0}
-                          max={Number(
-                            process.env.NEXT_PUBLIC_INTERVIEW_INDEX_D_MAX_MARKS,
-                          )}
-                          onChange={(e) =>
-                            Number(e.target.value) >
-                            Number(
-                              process.env
-                                .NEXT_PUBLIC_INTERVIEW_INDEX_D_MAX_MARKS,
-                            )
-                              ? null
-                              : setD(Number(e.target.value))
-                          }
-                          className="w-full rounded-xl p-2 mt-1"
-                        />
-                      </p>
-                    </div>
-                    <div className="flex justify-between my-5">
-                      <p className="text-red-850 text-lg sm:text-xl font-extrabold">
-                        Total Interview Marks (Out of{' '}
-                        {Number(
-                          process.env.NEXT_PUBLIC_INTERVIEW_INDEX_A_MAX_MARKS,
-                        ) +
-                          Number(
-                            process.env.NEXT_PUBLIC_INTERVIEW_INDEX_B_MAX_MARKS,
-                          ) +
-                          Number(
-                            process.env.NEXT_PUBLIC_INTERVIEW_INDEX_C_MAX_MARKS,
-                          ) +
-                          Number(
-                            process.env.NEXT_PUBLIC_INTERVIEW_INDEX_D_MAX_MARKS,
-                          ) +
-                          Number(
-                            process.env.NEXT_PUBLIC_INTERVIEW_INDEX_E_MAX_MARKS,
-                          )}
-                        )
-                      </p>
-                      <p className="sm:text-lg font-bold mr-10">
-                        {A + B + C + D}
-                      </p>
-                    </div>
-                    <div className="flex justify-center my-5">
-                      <button
-                        className={`text-white text-base md:text-lg py-1 px-3 rounded-lg ${
-                          adminPortalData.application_status >= 6 ||
-                          (!A && !B && !C && !D)
-                            ? 'bg-red-860 cursor-not-allowed'
-                            : 'bg-red-850'
-                        }`}
-                        onClick={() =>
-                          adminPortalData.application_status >= 6 ||
-                          (!A && !B && !C && !D)
-                            ? null
-                            : updateInterviewMarks(
-                                applId,
-                                authUser.id,
-                                A,
-                                B,
-                                C,
-                                D,
-                                5,
-                              )
-                                .then(() => {
-                                  alert('Succesfully Updated!')
-                                  setChangeOccured(!changeOccured)
-                                })
-                                .catch(() => alert('Try again, network error!'))
-                        }
-                      >
-                        Update Marks
-                      </button>
-                    </div>
-                  </div>
-                  <div className="bg-gray-200 rounded-3xl py-5 px-3 sm:py-10 sm:px-10 my-10">
-                    <div className="flex justify-between items-center my-5">
-                      <p className="text-red-850 text-lg sm:text-xl font-extrabold">
-                        Remark
-                      </p>
-                      <div className="sm:text-lg font-bold ml-20">
-                        <textarea
-                          name="Remark"
-                          rows={5}
-                          cols={100}
-                          value={remark}
-                          onChange={(e) => setRemark(e.target.value)}
-                          className="w-full rounded-xl p-2 mt-1"
-                        />
-                      </div>
-                    </div>
-                    <div className="flex justify-center my-5">
-                      <button
-                        className={`text-white text-base md:text-lg py-1 px-3 rounded-lg ${
-                          adminPortalData.application_status >= 6 || !remark
-                            ? 'bg-red-860 cursor-not-allowed'
-                            : 'bg-red-850'
-                        }`}
-                        onClick={() =>
-                          adminPortalData.application_status >= 6 || !remark
-                            ? null
-                            : updateInterviewRemark(applId, authUser.id, remark)
-                                .then(() => {
-                                  alert('Succesfully Updated!')
-                                  setChangeOccured(!changeOccured)
-                                })
-                                .catch(() => alert('Try again, network error!'))
-                        }
-                      >
-                        Update
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              ) : null}
-            </div>
-          ) : null}
-        </div>
+        status == 1 ? (
+          <div className="flex flex-col items-center mx-3 my-10 sm:m-10"></div>
+        ) : status == 2 ? (
+          <div className="flex flex-col items-center mx-3 my-10 sm:m-10"></div>
+        ) : status == 3 ? (
+          <div className="flex flex-col items-center mx-3 my-10 sm:m-10"></div>
+        ) : status == 4 ? (
+          <div className="flex flex-col items-center mx-3 my-10 sm:m-10"></div>
+        ) : status == 5 ? (
+          <div className="flex flex-col items-center mx-3 my-10 sm:m-10"></div>
+        ) : status == 6 ? (
+          <div className="flex flex-col items-center mx-3 my-10 sm:m-10"></div>
+        ) : status == 7 ? (
+          <div className="flex flex-col items-center mx-3 my-10 sm:m-10"></div>
+        ) : status == 8 ? (
+          <div className="flex flex-col items-center mx-3 my-10 sm:m-10"></div>
+        ) : (
+          <div className="flex justify-center items-center my-48">
+            <p className="text-xl text-red-850">
+              This application is already interviewed. You can still update the
+              marks.
+              <br />
+              Make sure to click on complete button on step 8, after making any
+              change.
+            </p>
+          </div>
+        )
       ) : (
         <Loading message="Loading your application data!" />
       )}
-    </InterviewerLayout>
+    </ApplicationLayout>
   )
 }
 
