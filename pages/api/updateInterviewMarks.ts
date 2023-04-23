8e4f35ae43194e8686a74054037d6631e5702d33
Himanshu <himvarandani@gmail.com>
2023-04-23 16:28:17 +0530

    Create interviewer view application step 8

--- pages/api/updateInterviewMarks.ts
+++ pages/api/updateInterviewMarks.ts
@@ -10,3 +10,26 @@ export const updateIntFormStatus = (
     [`interview_marks.${interviewerId}.formStatus`]: formStatus,
   })
 }
+
+export const updateInterviewMarks = (
+  userId: string,
+  interviewerId: string,
+  higherStudiesMot: number,
+  communication: number,
+  researchAptitude: number,
+  motivationToGoBack: number,
+  totalMarks: number,
+  remark: string,
+  formStatus: number,
+) => {
+  return firestore.doc(path.join('admin_portal_data', userId)).update({
+    [`interview_marks.${interviewerId}.higherStudiesMotivation`]:
+      higherStudiesMot,
+    [`interview_marks.${interviewerId}.communication`]: communication,
+    [`interview_marks.${interviewerId}.researchAptitude`]: researchAptitude,
+    [`interview_marks.${interviewerId}.motivationToGoBack`]: motivationToGoBack,
+    [`interview_marks.${interviewerId}.totalMarks`]: totalMarks,
+    [`interview_marks.${interviewerId}.remark`]: remark,
+    [`interview_marks.${interviewerId}.formStatus`]: formStatus,
+  })
+}
