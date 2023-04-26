6db8da77709885e15805fe62ce7c836f47c01efc
Himanshu Varandani <himvarandani@gmail.com>
2023-04-26 09:29:54 +0530

    Update Interviewer Confirmation Page (#234)
    Removing code which updates role from reviewer to interviewer in the interviewer confirmation page as it requires authentication to check the reviewer data

--- pages/interviewer/confirmation/[email].tsx
+++ pages/interviewer/confirmation/[email].tsx
@@ -4,8 +4,7 @@ import Roles from '../../../constants/roles'
 import { useAuth } from '../../../context/AuthUserContext'
 import firebase from '../../../firebase'
 import InterviewerLayout from '../../../layouts/interviewer'
-import { createUser, updateUserRole } from '../../api/createUser'
-import { getUserIdByEmail } from '../../api/getUserDetails'
+import { createUser } from '../../api/createUser'
 import {
   getInterviewerInvite,
   updateInterviewerResponse,
@@ -16,9 +15,6 @@ function InterviewerConfirmation() {
   const [password, setPassword] = useState<string>('')
   const [interviewerInvite, setInterviewerInvite] =
     useState<firebase.firestore.DocumentData>(null)
-  const [isReviewer, setIsReviewer] = useState<boolean>(false)
-  const [reviewerId, setReviewerId] = useState<string>()
-  const [loading, setLoading] = useState<boolean>(true)
   const [error, setError] = useState<string>('')
   const router = useRouter()
   const email = String(router.query['email'])
@@ -26,7 +22,6 @@ function InterviewerConfirmation() {
   useEffect(() => {
     if (!router.query['email']) return
 
-    setLoading(true)
     setError('')
     getInterviewerInvite(email)
       .then((invite) => {
@@ -39,85 +34,58 @@ function InterviewerConfirmation() {
         else setInterviewerInvite(invite)
       })
       .catch(() => setError('Network issue, Try Again!'))
-
-    // As User collection not set to accept many roles
-    // Updating Reviewer role Interviewer for now
-    // Need to update user collection to accept many roles in next session
-    getUserIdByEmail(email)
-      .then((userId: string | null) => {
-        if (!userId) setIsReviewer(false)
-        else {
-          setIsReviewer(true)
-          setReviewerId(userId)
-        }
-      })
-      .catch(() => setError('Network issue, Try Again!'))
-      .finally(() => setLoading(false))
   }, [email])
 
   const proceedYes = () => {
-    if (loading || !interviewerInvite || !interviewerInvite.name) return
+    if (!interviewerInvite || !interviewerInvite.name) return
 
     setError('')
-    if (!isReviewer) {
-      if (!password) {
-        setError('Please provide PASSWORD to create your account.')
-        return
-      }
-
-      createUserWithEmailAndPassword(email, password)
-        .then(async (result: firebase.auth.UserCredential) => {
-          result.user
-            .sendEmailVerification()
-            .then(() =>
-              alert(
-                'An email verification mail is sent to you. Please follow the instructions to verify your email.',
-              ),
-            )
-            .finally(() => {
-              // Update interviewer response to 'YES'
-              updateInterviewerResponse(email, 'YES')
-                .then(() => {
-                  // add interviewer data to firestore database
-                  // Mobile not required for now so giving 0 for now
-                  createUser(
-                    result.user.uid,
-                    interviewerInvite.name,
-                    email,
-                    0,
-                    new Date().getTime(),
-                    Roles.INTERVIEWER,
-                  )
-                })
-                .finally(() => {
-                  alert('Thanks for responding')
-                  router.push('/interviewer')
-                })
-            })
-        })
-        .catch((error) => {
-          if (error.code === 'auth/weak-password') {
-            setError('The password is too weak.')
-          } else {
-            setError(error.message.replace('Firebase', ''))
-          }
-        })
-    } else {
-      // Update interviewer response to 'YES'
-      updateInterviewerResponse(email, 'YES')
-        .then(() => {
-          // Update user role to Interviewer
-          updateUserRole(reviewerId, Roles.INTERVIEWER)
-        })
-        .finally(() => {
-          alert('Thanks for responding')
-          router.push('/interviewer')
-        })
+    if (!password) {
+      setError('Please provide PASSWORD to create your account.')
+      return
     }
+
+    createUserWithEmailAndPassword(email, password)
+      .then(async (result: firebase.auth.UserCredential) => {
+        result.user
+          .sendEmailVerification()
+          .then(() =>
+            alert(
+              'An email verification mail is sent to you. Please follow the instructions to verify your email.',
+            ),
+          )
+          .finally(() => {
+            // Update interviewer response to 'YES'
+            updateInterviewerResponse(email, 'YES')
+              .then(() => {
+                // add interviewer data to firestore database
+                // Mobile not required for now so giving 0 for now
+                createUser(
+                  result.user.uid,
+                  interviewerInvite.name,
+                  email,
+                  0,
+                  new Date().getTime(),
+                  Roles.INTERVIEWER,
+                )
+              })
+              .finally(() => {
+                alert('Thanks for responding')
+                router.push('/interviewer')
+              })
+          })
+      })
+      .catch((error) => {
+        if (error.code === 'auth/weak-password') {
+          setError('The password is too weak.')
+        } else {
+          setError(error.message.replace('Firebase', ''))
+        }
+      })
   }
 
   const proceedNo = () => {
-    if (loading || !interviewerInvite || !interviewerInvite.name) return
+    if (!interviewerInvite || !interviewerInvite.name) return
 
     updateInterviewerResponse(email, 'NO').then(() => {
       alert('Response Updated, Thank you!')
@@ -154,47 +122,36 @@ function InterviewerConfirmation() {
             </p>
           ) : null}
           <div className="flex flex-col items-center my-10">
-            {error || !isReviewer ? (
-              <input
-                name="Password"
-                placeholder="Provide your PASSWORD"
-                type="text"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="w-full rounded-xl p-2 mt-1 md:w-3/4 lg:w-1/2"
-              />
-            ) : (
-              <p className="text-blue-850 text-center pt-1">
-                No need of password, you can use your Reviewer account
-                credentials as an Interviewer if you click on &apos;YES&apos;
-              </p>
-            )}
+            <input
+              name="Password"
+              placeholder="Provide your PASSWORD"
+              type="text"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full rounded-xl p-2 mt-1 md:w-3/4 lg:w-1/2"
+            />
             <div className="flex mt-10">
               <button
                 className={`text-white text-base md:text-lg rounded-lg ${
-                  loading || !interviewerInvite || !interviewerInvite.name
+                  !interviewerInvite || !interviewerInvite.name
                     ? 'bg-red-860 cursor-not-allowed'
                     : 'bg-red-850'
                 } py-2 px-5 order-1 sm:order-2
                 `}
                 onClick={proceedYes}
-                disabled={
-                  loading || !interviewerInvite || !interviewerInvite.name
-                }
+                disabled={!interviewerInvite || !interviewerInvite.name}
               >
                 YES
               </button>
               <button
                 className={`text-white text-base md:text-lg rounded-lg ${
-                  loading || !interviewerInvite || !interviewerInvite.name
+                  !interviewerInvite || !interviewerInvite.name
                     ? 'bg-red-860 cursor-not-allowed'
                     : 'bg-red-850'
                 } ml-5 py-2 px-5 order-1 sm:order-2
                 `}
                 onClick={proceedNo}
-                disabled={
-                  loading || !interviewerInvite || !interviewerInvite.name
-                }
+                disabled={!interviewerInvite || !interviewerInvite.name}
               >
                 NO
               </button>
