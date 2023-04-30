292b4be1438885c87d81bac821fa1a0e44b2fe1d
Sumir Vats <sumirvats@gmail.com>
2023-04-30 11:32:13 +0530

    Updated Interviewer instructions

--- pages/interviewer/index.tsx
+++ pages/interviewer/index.tsx
@@ -16,53 +16,54 @@ function InterviewerPortal() {
             interviewed based on the following rubric:
           </p>
           <p className="my-5 text-lg font-bold italic">
-            Technical questions (30 points)
+            Motivation for higher studies (30 Points)
           </p>
           <p className="my-5 text-lg text-blue-850">
-            Candidate&apos;s knowledge about their field of study. Specifically,
-            please try to assess fundamental concepts of their subject which
-            would be critical for them in their higher studies.
+            We encourage you to probe the students in detail about their plans.
+            Specifically, please try to figure out where higher education abroad
+            stands in their priority list as opposed to taking up alternate
+            career options upon completion of their current degree. Motivation
+            also gauges their genuine love for the subject instead of a wish to
+            travel abroad or getting this scholarship just for improving their
+            CVs. Please do your best to assess if they will be able to persevere
+            in the arduous process of the applications, and later the grad
+            school.
           </p>
           <p className="my-5 text-lg font-bold italic">
-            Language and clarity of thought (20 points)
+            Communication Skills (30 Points)
           </p>
           <p className="my-5 text-lg text-blue-850">
-            Basic English language proficiency/fluency. Specifically, please try
-            to assess if they are equipped to prepare their application package
-            and appear for an interview with their prospective supervisor
-            without major flaws in language. However, it does not mean that any
-            special literary/oratory skills have to be rewarded.
+            A candidate must have basic English language proficiency/fluency.
+            Specifically, please try to assess if they are equipped to prepare
+            their application package and appear for an interview with their
+            prospective supervisor without major flaws in language. However, it
+            does not mean that any special literary/oratory skills have to be
+            rewarded.
           </p>
           <p className="my-5 text-lg font-bold italic">
-            Motivation for higher studies (40 points)
+            Academic of Research Aptitude (30 Points)
           </p>
           <p className="my-5 text-lg text-blue-850">
-            Candidate&apos;s motivation for higher studies abroad. We encourage
-            you to probe the students in detail about their plans. Specifically,
-            please try to figure out where higher education abroad stands in
-            their priority list as opposed to taking up alternate career options
-            upon completion of their current degree. Motivation also gauges
-            their genuine love for the subject instead of a wish to travel
-            abroad or getting this scholarship just for improving their CVs.
-            Please do your best to assess if they will be able to persevere in
-            the arduous process of the applications, and later the grad school.
+            We need to assess the candidate's knowledge about their field of
+            study or the field of research they are interested in or have done
+            in the past. Specifically, please try to assess fundamental concepts
+            of their subject which would be critical for them in their higher
+            studies.
           </p>
           <p className="my-5 text-lg font-bold italic">
-            Overall personality (10 points)
+            Motivation to Give Back (10 Points)
           </p>
           <p className="my-5 text-lg text-blue-850">
-            This is a bit of an open-ended section to reward any unique quality
-            that you may observe in the candidate. As the name of this category
-            implies, please gauge the personality of the candidate on an
-            all-round basis.
+            The candidate should be motivated to give back to their community
+            and alma-mater with a consistent track record of leadership roles
+            and helping others.
           </p>
           <h2 className="mt-10 mb-5 text-2xl font-bold">Recording Points</h2>
           <p className="my-5 text-lg text-blue-850">
             After interviewing each candidate, each panelist is expected to
             record the points based on their own impartial and rational judgment
-            of the candidate&apos;s aforementioned abilities. To this end, use
-            our web-portal where you can easily enter and record the points.
-            This has been already discussed in item-2: Candidate Background. We
+            of the candidate's aforementioned abilities. To this end, use our
+            web-portal where you can easily enter and record the points. We
             recommend you to record the points on an excel sheet or a piece of
             paper, as a backup.
           </p>
