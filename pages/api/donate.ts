fd4ceeb1c651e75542155ad5a995b6895c0fe300
Dhruv Maheshwari <68808611+dhruvm2509@users.noreply.github.com>
2023-11-10 09:01:48 +0530

    Deleted monthly recurring button from donate page (#255)
    * deleted monthly recurring button
    
    * resolved error in donate page
    
    * removed a line from donate page
    
    * removed line from donate page

--- pages/api/donate.ts
+++ pages/api/donate.ts
@@ -4,15 +4,15 @@ import { firestore } from '../../firebase'
 export const addDonation = (
   name: string,
   email: string,
-  donationType: string,
-  amount: number,
+  // donationType: string,
+  // amount: number,
   paymentType: string,
 ) => {
   return firestore.doc(path.join('donations', email)).set({
     name,
     email,
-    donationType,
-    amount,
+    // donationType,
+    // amount,
     paymentType,
   })
 }
