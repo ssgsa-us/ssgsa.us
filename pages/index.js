a12b1d06e254d90e733b89d12cb4a842e880761b
Himanshu <himvarandani@gmail.com>
2024-02-11 00:16:46 +0530

    Update marquee applications open

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,9 @@ export default function Home() {
     <MainLayout>
       <div className="bg-gray-350 text-red-850 font-black w-screen -ml-2 sm:-ml-5 md:-ml-10 lg:-ml-20 xl:-ml-40 mt-8 lg:mr-2 lg:p-3 p-2">
         <marquee className="flex items-center">
-          Applications for SSGSA 2024-2025 will be announced soon
+          <Link href="/application-portal">
+            <a>Application will be open on Feb 11 at 8:30 PM IST</a>
+          </Link>
         </marquee>
       </div>
       <Counter />
