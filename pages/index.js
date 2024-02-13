2894cf04caa9ce259351ba12bd92006ad033a9b4
Himanshu Varandani <himvarandani@gmail.com>
2024-02-14 00:32:07 +0530

    Update ticker message

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       <div className="bg-gray-350 text-red-850 font-black w-screen -ml-2 sm:-ml-5 md:-ml-10 lg:-ml-20 xl:-ml-40 mt-8 lg:mr-2 lg:p-3 p-2">
         <marquee className="flex items-center">
           <Link href="/application-portal">
-            <a>Application will be open on Feb 11 at 8:30 PM IST</a>
+            <a>Click here to apply</a>
           </Link>
         </marquee>
       </div>
