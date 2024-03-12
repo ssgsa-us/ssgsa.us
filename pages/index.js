3226759389b03b8c92a960ea702b6cec73916c6b
Himanshu Varandani <himvarandani@gmail.com>
2024-03-12 09:39:30 +0530

    Update ticker message to extend deadline (#265)
    * Update ticker message to extend deadline
    
    * Apply link to full message

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       <div className="bg-gray-350 text-red-850 font-black w-screen -ml-2 sm:-ml-5 md:-ml-10 lg:-ml-20 xl:-ml-40 mt-8 lg:mr-2 lg:p-3 p-2">
         <marquee className="flex items-center">
           <Link href="/application-portal">
-            <a>Click here to apply</a>
+            <a>Deadline for application has been EXTENDED to March 12, 2024 (11:59 pm IST). Click here to apply.</a>
           </Link>
         </marquee>
       </div>
