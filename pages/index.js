b6788fa03879a37011973119df367b10d3f3d154
Himanshu Varandani <himvarandani@gmail.com>
2024-03-29 23:56:51 +0530

    Update ticker message to application closed

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,7 @@ export default function Home() {
     <MainLayout>
       <div className="bg-gray-350 text-red-850 font-black w-screen -ml-2 sm:-ml-5 md:-ml-10 lg:-ml-20 xl:-ml-40 mt-8 lg:mr-2 lg:p-3 p-2">
         <marquee className="flex items-center">
-          <Link href="/application-portal">
-            <a>Deadline for application has been EXTENDED to March 12, 2024 (11:59 pm IST). Click here to apply.</a>
-          </Link>
+          Applications for session 2024-25 are now closed.
         </marquee>
       </div>
       <Counter />
