9c5e6f143ac5588021afd356ef9e834a5ad4f478
Himanshu <himvarandani@gmail.com>
2025-02-15 02:01:05 +0530

    fix: update policies and procedures

--- pages/policiesAndProcedures.tsx
+++ pages/policiesAndProcedures.tsx
@@ -18,156 +18,114 @@ export default function Home() {
                 Award &#40;SSGSA&#41;. SSGSA, formerly known as the Sir Syed
                 Excellence in Science Award &#40;SSESA&#41;, is a merit-based
                 scholarship that is awarded to academically outstanding students
-                from all faculties at the Aligarh Muslim University. All
-                donations and contributions directly sponsor the attempts of
-                talented students to secure admission in MS/Ph.D programs at
-                centers/universities of international repute. SSGSA supports
-                each scholar in achieving their full potential, as outlined
-                below:
-              </p>
-              <ul
-                className="my-2 pl-8 sm:pl-12 sm:pr-5 md:pl-24 md:pr-10"
-                style={{ listStyle: 'disc' }}
-              >
-                <li className="my-2">
-                  SSGSA scholars are selected on the basis of rigorous review of
-                  their application by independent academic experts and personal
-                  interview to assess knowledge and motivation.
-                </li>
-                <li className="my-2">
-                  A committed team of mentors work with the scholars on every
-                  aspect of their preparation and application. The mentors
-                  continue to be available for guidance and help throughout the
-                  academic journey of the scholars, and in many cases well into
-                  their professional careers.
-                </li>
-                <li className="my-2">
-                  Upon successful admission, the students in turn become mentors
-                  to other students.
-                </li>
-              </ul>
-            </div>
-            <div className="my-5">
-              <h2 className="mb-2 text-lg font-bold">Points of Expenditure:</h2>
-              <p className="pl-2 sm:px-5 md:px-10">
-                There are three main parts of expenditure incurred by
-                prospective applicants. GRE/GMAT and TOEFL/IELTS, mandatory for
-                application to most masters and Ph.D. programs in the United
-                States, and individual application fee of the university.
+                from all faculties at the Aligarh Muslim University, India.
+                SSGSA helps awardees in applying for MS/PhD programs at
+                centers/universities of international repute.
               </p>
             </div>
             <div className="my-5">
               <h2 className="mb-2 text-lg font-bold">Award Benefits:</h2>
               <p className="pl-2 sm:px-5 md:px-10">
-                SSGSA helps scholars with financial assistance towards
-                standardized test-preparation-materials, standardized tests
-                &#40;GRE &amp; TOEFL&#41; and application fees, and personalized
-                mentoring throughout the process from selection of universities
-                to refining personal essays and resumes.
-              </p>
-            </div>
-            <div className="my-5">
-              <h2 className="mb-2 text-lg font-bold">Sponsorship by SSGSA:</h2>
-              <p className="pl-2 sm:px-5 md:px-10">
-                SSGSA covers the cost of GRE/GMAT and TOEFL/IELTS entirely, and
-                funds the application fee of up to 5 universities. No cash is
-                available to the student at any point during the entire
-                application procedure. GRE/GMAT, TOEFL/IELTS as well as
-                application fees are paid online by the SSGSA operational team.
-                In order to ensure serious effort by the students that are
-                nominated, we pay for the tests in steps. For example, students
-                who get a good score in GRE &#40;over 300, as recommended by
-                most universities&#41; will be supported for TOEFL, and students
-                who score well in both the tests will be supported for the
-                application fees for the universities. Students who don&apos;t
-                get good scores will not be supported for the next step. Each
-                candidate is supported for only one attempt. Scholars are
-                advised to take practice tests and send those scores to our
-                mentorship team for evaluation. Based on how they perform in the
-                practice tests, a decision on whether to pay for the tests will
-                be made.
-              </p>
-            </div>
-            <div className="my-5">
-              <h2 className="mb-2 text-lg font-bold">Selection of Awardees:</h2>
-              <p className="pl-2 sm:px-5 md:px-10">
-                Since the existing examination system in India &#40;at the
-                bachelor&apos;s and master&apos;s level and the other entrance
-                exams for Ph.D.&#41; is entirely different from the TOEFL/IELTS/
-                GRE/GMAT pattern, it may not be possible for students to prepare
-                well for these tests simultaneously. Hence, the award is valid
-                for two years. Applications for the award are invited in the
-                first week of February and results are declared by the third
-                week of March. The selection is based only on merit.
-                Applications from talented students with a proven academic
-                record are considered. However, there are certain other criteria
-                that the student has to fulfill. The entire application
-                procedure entails the following requirements:
+                SSGSA helps selected awardees with 1-to-1 personalized mentoring
+                throughout the application process starting from selection of
+                universities to refining personal essays and resumes and
+                financial support that includes the following:
               </p>
               <ul
                 className="my-2 pl-8 sm:pl-12 sm:pr-5 md:pl-24 md:pr-10"
                 style={{ listStyle: 'disc' }}
               >
-                <li className="my-2">Curriculum Vitae/Resume</li>
                 <li className="my-2">
-                  Attested copy of mark sheets for all years of Std. XII, B.Sc
-                  and M.Sc. &#40;NET, GATE and other test scores may also be
-                  included if taken&#41;.
+                  Preparation material for standardised exams
                 </li>
                 <li className="my-2">
-                  Attested copies of co-curricular and extra-curricular
-                  certificates and documents in support of listed achievements.
+                  One out of GRE or GMAT or any other equivalent exam
+                </li>
+                <li className="my-2">One out of TOEFL or IELTS</li>
+                <li className="my-2">Score reports up to five universities</li>
+                <li className="my-2">
+                  Application fees up to five universities
+                </li>
+              </ul>
+              <p className="pl-2 sm:px-5 md:px-10">
+                Please note that no cash is available to the student at any
+                point during the entire application procedure and SSGSA submits
+                the fees at its own end. Also, in order to ensure serious effort
+                by the scholars, we pay for the tests once they get a minimum
+                score in at least two practice tests, as recommended by SSGSA.
+              </p>
+            </div>
+            <div className="my-5">
+              <h2 className="mb-2 text-lg font-bold">
+                Application Requirements
+              </h2>
+              <p className="pl-2 sm:px-5 md:px-10">
+                Applications for the award are typically invited in the month of
+                February and results are declared in May. The selection is
+                purely based only on merit. The entire application procedure
+                entails the following requirements:
+              </p>
+              <ul
+                className="my-2 pl-8 sm:pl-12 sm:pr-5 md:pl-24 md:pr-10"
+                style={{ listStyle: 'disc' }}
+              >
+                <li className="my-2">
+                  Personal information based on Curriculum Vitae/Resume
                 </li>
                 <li className="my-2">
-                  Responses to essay-type questions, briefing the student&apos;s
-                  interest in their intended field of study &#40;this is
-                  important as it is also one of the selection criteria in the
-                  US graduate programs and will help us evaluate the
-                  student&#41;, motivation, leadership, initiative, creativity
-                  etc.
+                  Copy of mark sheets for all years of bachelors and masters
+                  degree.
+                </li>
+                <li className="my-2">
+                  Copies of co-curricular and extra-curricular certificates and
+                  documents in support of listed achievements.
+                </li>
+                <li className="my-2">
+                  Responses to essay-type questions &#40;similar to US graduate
+                  school applications&#41;.
                 </li>
               </ul>
             </div>
             <div className="my-5">
-              <h2 className="mb-2 text-lg font-bold">
-                Selection of the students:
-              </h2>
+              <h2 className="mb-2 text-lg font-bold">Selection of Awardees:</h2>
               <p className="pl-2 sm:px-5 md:px-10">
-                A panel of experts in the respective fields review the contents
-                of each application from that discipline. The reviews are done
-                independently so that the students get a fair assessment, and
-                each application is assigned points on a scale of 1-100. An
-                average of these points is used to draw up a merit list and the
-                top 20% students are called for a personal interview round.
-                About 25-30 students &#40;dependent on available financial
-                aid&#41; are awarded SSGSA scholarships each year.
+                Scholars are selected on the basis of rigorous review of their
+                application by independent academic experts and followed by
+                personal interviews to assess knowledge and motivation. About
+                25-30 students &#40;dependent on available financial aid&#41;
+                are awarded SSGSA scholarships each year.
               </p>
             </div>
             <div className="my-5">
               <h2 className="mb-2 text-lg font-bold">Mentorship:</h2>
               <p className="pl-2 sm:px-5 md:px-10">
-                Besides the financial assistance required for the GRE/GMAT,
-                TOEFL/IELTS, and application fees that are needed for applying
-                to the US universities, the awardees are assigned mentors who
-                are experts in the field. The scholars are guided through the
-                entire application process and receive help in framing a strong
-                personal statement, resume etc.
+                In addition to financial support, awardees are paired with
+                expert personal mentors who guide them throughout the entire
+                application process. These mentors provide personalized advice
+                on crafting compelling personal statements, refining resumes,
+                and building a strong overall application package.
               </p>
-            </div>
-            <div className="my-5">
-              <h2 className="mb-2 text-lg font-bold">Validity:</h2>
               <p className="pl-2 sm:px-5 md:px-10">
-                The award and its benefits are valid for a maximum period of two
-                years from the date of announcement. Students must take the
-                exams and apply for admission within this time. Failure to do so
-                would automatically disqualify the student from the award. Under
-                pressing/extenuating circumstances, the student may request for
-                extending the award benefits. However, the committee&apos;s
-                decision will be final.
+                Upon successful admission, the students in turn become mentors
+                to other students.
               </p>
             </div>
             <div className="my-5">
-              <h2 className="mb-2 text-lg font-bold">Disqualifications:</h2>
+              <h2 className="mb-2 text-lg font-bold">Validity of the award:</h2>
+              <p className="pl-2 sm:px-5 md:px-10">
+                The award and its benefits are valid for a period of 1 year from
+                the date of announcement of the award. Students must take the
+                exams and apply for admission within this time. Failure to do so
+                would automatically disqualify the student from the award.
+                Extensions to the award—up to one year—may be granted under
+                extenuating circumstances, subject to approval by the SSGSA
+                Executive Team.
+              </p>
+            </div>
+            <div className="my-5">
+              <h2 className="mb-2 text-lg font-bold">
+                Conditions for disqualification
+              </h2>
               <p className="pl-2 sm:px-5 md:px-10">
                 Students will face immediate disqualification under the
                 following conditions:
@@ -182,52 +140,34 @@ export default function Home() {
                   due to an act of plagiarism.
                 </li>
                 <li className="my-2">
-                  If the student gets a score in the exams that is not enough to
-                  make the application competitive. For GRE, at least 300/340
-                  and for TOEFL at least 90/120 would be the requirement. They
-                  will be given a chance to take the exam again AT THEIR OWN
-                  EXPENSE. The award benefits will be re-offered if they score
-                  well on the second attempt.
-                </li>
-                <li className="my-2">
                   The students are required to update the committee on their
                   progress at regular intervals &#40;e.g., monthly updates&#41;.
                   If they fail to do so or attempts to contact them prove futile
                   repeatedly, they will be given a notice to respond or face
                   disqualification.
                 </li>
+                <li className="my-2">
+                  Awardees are prohibited from participating in or benefiting
+                  from programs similar to SSGSA during the award period.
+                </li>
               </ul>
-            </div>
-            <div className="my-5">
-              <h2 className="mb-2 text-lg font-bold">Internal Growth:</h2>
-              <p className="pl-2 sm:px-5 md:px-10">
-                Students/scholars, once successfully sponsored, will
-                automatically become paying members of the SSGSA fraternity.
-                They will commit to returning the initial scholarship awarded to
-                them in this manner. The students, who have benefited from the
-                SSGSA award and have complete faith in its existence, will bring
-                a fresh perspective and spirit to contribute to the viability
-                and growth of this mission. Successful awardees will be involved
-                in the functioning of the program once they come to the US or
-                Europe.
-              </p>
             </div>
 
             <div className="my-5">
               <h2 className="mb-2 text-lg font-bold">
-                SSGSA Policies and Procedures for 2025
+                New policies starting from 2025 &#40;to emphasize&#41;
               </h2>
               <ul
                 style={{ listStyleType: 'disc' }}
                 className="pl-2 sm:px-5 md:px-10"
               >
                 <li className="my-2">
-                  The validity of the award will be for only one year (May 2025
-                  to May 2026). We recommend that you apply for the award only
-                  if you can commit at least 4 hours per week on preparing your
-                  university applications this year. Extensions to the award (up
-                  to 1 year) may be granted subject to approval from the mentors
-                  and SSGSA mentorship team.
+                  The validity of the award will be for only one year &#40;May
+                  2025 to May 2026&#41;. We recommend that you apply for the
+                  award only if you can commit at least 4 hours per week on
+                  preparing your university applications this year. Extensions
+                  to the award &#40;up to 1 year&#41; may be granted subject to
+                  approval from the mentors and SSGSA mentorship team.
                 </li>
                 <li className="my-2">
                   The SSGSA Award will initially be offered on a provisional
@@ -240,7 +180,7 @@ export default function Home() {
                   <ul style={{ listStyleType: 'circle' }} className="pl-5">
                     <li className="my-2">
                       Creation of a university application timeline and plan
-                      with your assigned SSGSA mentors.
+                      with your assigned SSGSA mentors
                     </li>
                     <li className="my-2">
                       Regular and effective communication with SSGSA mentors.
@@ -262,10 +202,6 @@ export default function Home() {
                   Executive Committee retains the sole discretion to revoke any
                   support provided by the organization at any time and under any
                   circumstances it deems necessary.
-                </li>
-                <li className="my-2">
-                  Please refer to the SSGSA policies and procedures page for a
-                  complete list of policies.
                 </li>
               </ul>
             </div>
