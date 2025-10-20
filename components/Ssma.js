1cb970cfb75866b36ab6c1018cded9a209dcd7c7
Sabahuddin Ahmad <sabahuddin@live.in>
2025-10-20 22:35:10 +0200

    Create Ssma.js

---
+++ components/Ssma.js
@@ -0,0 +1,53 @@
+const Ssma = () => {
+  return (
+    <div className="text-black">
+      <h1 className="mb-8 bg-blue-850 text-xl lg:text-2xl text-white text-center font-extrabold py-2 px-2 rounded-tl-3xl rounded-br-3xl">
+        About the Simmone Siddiqui Memorial Award (SSMA)
+      </h1>
+
+      <div className="mx-4 sm:mx-8 md:mx-16">
+        <p>
+          The Simmone Siddiqui Memorial Award (SSMA) is a travel award, and was
+          established by the family of Late Ms. Simmone Siddiqui in her memory
+          to support recipients of the Sir Syed Global Scholar Award (SSGSA) who
+          intend to pursue graduate studies abroad. The award provides up to
+          $1000 to assist one SSGSA Scholar with one-time economy-class air
+          travel expenses from their current country of residence to the country
+          where they will begin their graduate program. The amount will not be
+          transferred directly to the awardee; instead, the SSGSA will
+          facilitate the flight booking once the student has received a valid
+          student visa. Awardees are encouraged to contribute back to SSGSA in
+          the future to help sustain this meaningful initiative.
+        </p>
+        <br />
+        <p>
+          To be eligible for the SSMA, applicants must be SSGSA Scholars who
+          have secured admission to a graduate (Master’s or Ph.D.) program at a
+          prestigious university outside India. The program must commence in the
+          Fall session of the award year (for example, Fall 2025 for the 2025
+          award cycle). Only those who will begin their studies abroad and meet
+          these criteria will be considered for the award.
+        </p>
+        <br />
+        <p>
+          Applicants are required to submit their application along with the
+          following documents: (1) the most recent CV, (2) a family income
+          certificate, salary slip, or other official document verifying family
+          income, (3) a list of all university admissions, specifying the
+          university name, program name, and whether the program is funded or
+          self-funded, and (4) a 200-word essay expressing the need for the SSMA
+          Award.
+        </p>
+        <br />
+        <p>
+          Eligible candidates will be contacted directly by the SSGSA with
+          details about the application timeline and submission deadline. All
+          applications will be reviewed by the SSGSA Executive Team, and the
+          recipient will be selected based on merit and need.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Ssma
