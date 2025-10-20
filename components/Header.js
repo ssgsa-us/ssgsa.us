f446cb234c7ca828e73e882a86bbd36c105f3d22
Sabahuddin Ahmad <sabahuddin@live.in>
2025-10-20 23:05:33 +0200

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -257,6 +257,17 @@ export default function Header() {
                 <a className="py-4 px-2 text-white text-sm">DONATE</a>
               </Link>
             </div>
+            <div
+              className={`flex flex-col justify-center h-full hover:bg-blue-850 ${
+                router.pathname == '/ssma' && 'bg-blue-850'
+              }`}
+            >
+              <Link href="/ssma">
+                <a className="px-2 md:py-4 md:px-1 text-white text-sm">
+                  Simmone Siddiqui Memorial Award (SSMA)
+                </a>
+               </Link>
+              </div>
             {/* <div
               className={`flex flex-col justify-center h-full hover:bg-blue-850 ${
                 router.pathname == '/isra' && 'bg-blue-850'
