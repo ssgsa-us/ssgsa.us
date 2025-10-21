cfef2b96c108d93db66b93e5134290dc9aa90953
Himanshu Varandani <himvarandani@gmail.com>
2025-10-21 10:04:45 +0530

    Create Ssma component

---
+++ components/Ssma.js
@@ -0,0 +1,32 @@
+const Ssma = () => {
+  return (
+    <div className="text-black">
+      <h1 className="mb-8 bg-blue-850 text-xl lg:text-2xl text-white text-center font-extrabold py-2 px-2 rounded-tl-3xl rounded-br-3xl">
+        About the Simmone Siddiqui Memorial Award (SSMA)
+      </h1>
+
+      <div className="mx-4 sm:mx-8 md:mx-16">
+        <p>
+          The Simmone Siddiqui Memorial Award (SSMA) is an excellent
+          opportunity for young graduates, who wish to work for 2-3 months in
+          the academic environment of prestigious universities of the United
+          States of America (USA).
+        </p>
+        <br />
+        <p>
+          The program aims to give awardees a short exposure to the cutting-edge
+          research environment and work culture of the US. Unlike the Sir Syed
+          Global Scholar Award, (which is also administered by Sir Syed
+          Education Society of North America) which helps you in applying for
+          different universities in the US through significant financial
+          assistance coupled with thorough mentoring, the ISRA provides a closer
+          look, helping the aspirants from being familiarized with the research
+          milieu of USA thereby boosting their application according to their
+          training.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Ssma
