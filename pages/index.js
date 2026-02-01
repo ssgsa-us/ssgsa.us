c33380b8c8e9d1f08c1f71a4877d7e63d95b690c
Himanshu Varandani <himvarandani@gmail.com>
2026-02-01 22:53:02 +0530

    Update session in application banner

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
     <MainLayout>
       <div className="mt-8">
         <Banner
-          message="Applications for session 2025-26 are open now. Click here to apply!"
+          message="Applications for session 2026-27 are open now. Click here to apply!"
           link="/application-portal"
         />
       </div>
